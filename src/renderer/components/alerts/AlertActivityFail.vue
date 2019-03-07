535f6681623571435ad64ad5a00697134aef01e7
Damis Garcia <damisgarcia@gmail.com>
2019-03-07 18:34:35 -0300

    Update hide and show alerts

--- src/renderer/components/alerts/AlertActivityFail.vue
+++ src/renderer/components/alerts/AlertActivityFail.vue
@@ -1,14 +1,35 @@
 <template>
-    <b-modal v-model="Alert.isVisibleActivityAlertFail" title="Falha" @hidden="resetAnswer">Tente novamente!</b-modal>
+    <b-modal ref="modal" title="Falha" @hidden="onHidden">
+        <p>Tente novamente!</p>
+        <template slot="modal-ok">Ops :(</template>
+        <template slot="modal-cancel">Fechar</template>
+    </b-modal>
 </template>
 <script>
 import { mapState, mapActions } from 'vuex'
 export default {
+    data(){
+        return {
+            isVisible: false
+        }
+    },
     computed: {
-        ...mapState(['Alert'])
+        ...mapState({
+            isVisibleActivityAlertFail: state => state.Alert.isVisibleActivityAlertFail
+        })
     },
     methods: {
+        onHidden(){
+            this.hideAlertActivityFail()
+            this.resetAnswer()
+        },
+        ...mapActions(['hideAlertActivityFail']),
         ...mapActions('Activity', ['resetAnswer'])
+    },
+    watch: {
+        isVisibleActivityAlertFail(value){
+            value ? this.$refs.modal.show() : this.$refs.modal.hide()
+        }
     }
 }
 </script>
--- src/renderer/components/alerts/AlertActivitySuccess.vue
+++ src/renderer/components/alerts/AlertActivitySuccess.vue
@@ -1,15 +1,35 @@
 <template>
-    <b-modal v-model="Alert.isVisibleActivityAlertSuccess" title="Sucesso" @hidden="resetAnswer">Parabéns tudo certo!</b-modal>
+    <b-modal ref="modal" title="Sucesso" @hidden="onHidden">
+        <p>Parabéns tudo certo!</p>
+        <template slot="modal-ok">Urru!</template>
+        <template slot="modal-cancel">Fechar</template>
+    </b-modal>
 </template>
 <script>
 import { mapState, mapActions } from 'vuex'
 export default {
+    data(){
+        return {
+            isVisible: false
+        }
+    },
     computed: {
-        ...mapState(['Alert'])
+        ...mapState({
+            isVisibleActivityAlertSuccess: state => state.Alert.isVisibleActivityAlertSuccess
+        })
     },
     methods: {
+        onHidden(){
+            this.hideAlertActivitySuccess()
+            this.resetAnswer()
+        },
+        ...mapActions(['hideAlertActivitySuccess']),
         ...mapActions('Activity', ['resetAnswer'])
+    },
+    watch: {
+        isVisibleActivityAlertSuccess(value){
+            value ? this.$refs.modal.show() : this.$refs.modal.hide()
+        }
     }
 }
 </script>
-
