56b44a3fc77c78defe58ce627fed7f4129708d3e
Damis Garcia <damisgarcia@gmail.com>
2019-04-03 11:05:25 -0300

    move helpers functions in Activity reducer for helpers/index.js

--- src/renderer/store/modules/Activity.js
+++ src/renderer/store/modules/Activity.js
@@ -1,9 +1,16 @@
 import Vue from 'vue'
-import { filter, find, findIndex, clone } from 'lodash'
+import { filter, find, clone } from 'lodash'
 import API from '@/services/Http'
-import qs from 'qs'
 
-import { ClusterTypes, PointingsTypes, initialStateLog, MaxStars } from './helpers'
+import { 
+    ClusterTypes, 
+    PointingsTypes, 
+    initialStateLog, 
+    MaxStars,
+    validationInAnswer,
+    validationInSelection,
+    getExtenalParams
+} from './helpers'
 
 
 const state = {
@@ -29,13 +36,7 @@ const mutations = {
 
     INCREMENT_TIMER(state, activity){
       state.log.timer.totalSeconds += 1  
-    },
-
-    REGISTER_ANSWER(state, { type, data }) { // ref= uid_response, type_response, id_response
-        // let indexOf = findIndex(state.answer, { ref: ref })
-        // state.answer[indexOf][key].data = data
-        state.selection[type] = data
-    },
+    },    
 
     COMPUTED_ANSWER(state, indexOfAnswer){
         let answer = clone(state.answer)
@@ -141,79 +142,6 @@ const actions = {
 
     incrementTimer({ commit }){
         commit('INCREMENT_TIMER')
-    }
-}
-
-// Validations
-function validationInAnswer({ state, dispatch, commit }, { vm, type, data }){
-    let indexOfAnswer = findIndex(state.answer, a => a.value.data.includes(data))
-
-    if (indexOfAnswer === -1) {
-        vm.invalid = true
-        // register total error
-        commit('TRIGGER_FAIL')
-    } else {
-        // notify user feedback
-        vm.valid = true
-
-        commit('COMPUTED_ANSWER', indexOfAnswer)
-    }
-}
-
-function validationInSelection({ state, dispatch, commit }, { vm, type, data }){
-    let { selection } = state
-
-    selection[type] = {
-        data,
-        vm
-    }
-
-    vm.selected = true
-
-    if (selection.key && selection.value) {
-        let indexOfAnswer = findIndex(state.answer, a => a.key.data === selection.key.data)
-        let isCorrect = ( indexOfAnswer !== -1 ) && state.answer[indexOfAnswer].value.data.includes(selection.value.data)
-        
-        if (isCorrect){
-            // notify user feedback
-            selection.key.vm.valid = true
-            selection.value.vm.valid = true
-            
-            commit('CLEAR_SELECTION')
-            commit('COMPUTED_ANSWER', indexOfAnswer)
-        } else {
-            // notify user feedback
-            selection.key.vm.invalid = true
-            selection.value.vm.invalid = true
-
-            commit('CLEAR_SELECTION')
-            commit('TRIGGER_FAIL')
-        }
-    }
-}
-
-
-
-// Helpers
-function getExtenalParams(question){
-    let external_params = []
-
-    switch (question.external_param_type) {
-        case 'substantivo_proprio':
-            external_params.push({ 'name': 'Ronaldo' })
-            break;    
-        default:
-            break;
-    }
-    if (external_params.length) {
-        return {
-            params: { external_params },
-            paramsSerializer: (params) => {
-                return qs.stringify(params, { encode: false });
-            }
-        }
-    } else { 
-        return { params: null }
     }
 }
 
