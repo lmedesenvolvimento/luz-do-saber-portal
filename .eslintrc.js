c7c3b17a041a379b0828396f11a2681ec966e321
Damis Garcia <damisgarcia@gmail.com>
2019-04-03 13:16:58 -0300

    finish configuration eslint

---
+++ .eslintrc.js
@@ -0,0 +1,16 @@
+module.exports = {
+    extends: [
+        // add more generic rulesets here, such as:
+        // 'eslint:recommended',
+        'plugin:vue/recommended'
+    ],
+    rules: {
+        // override/add rules settings here, such as:
+        'vue/no-unused-vars': 'error',
+        'vue/html-indent': ['error', 4],
+        'vue/max-attributes-per-line': 'off',
+        'vue/singleline-html-element-content-newline': 'off',
+        'quotes': ['error', 'single'],
+        'indent': ['error', 4]
+    }
+}
