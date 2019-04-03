4008451376f8640866b726abd7c24519fa5f57fa
eduardo <eduregis1@gmail.com>
2019-04-03 10:54:47 -0300

    finish feedback layout

--- src/renderer/components/alerts/AlertActivitySuccess.vue
+++ src/renderer/components/alerts/AlertActivitySuccess.vue
@@ -1,15 +1,63 @@
 <template>
-    <b-modal ref="modal" title="Sucesso" @hidden="onHidden">
-        <p>Parabéns tudo certo!</p>
-        <template slot="modal-ok">Urru!</template>
-        <template slot="modal-cancel">Fechar</template>
-    </b-modal>
+    <div>
+        <b-modal :centered="true" content-class="feedback" :hide-footer="true" v-model="isVisible">
+            <template slot="modal-header">
+                <div class="feedback-header">
+                     <div class="feedback-stars feedback-header-item">
+                        <img v-if="stars<1" :src="require('@/assets/images/components/feedback/star-empty.png')" class="feedback-small-stars" alt="star-empty"> 
+                        <img v-else :src="require('@/assets/images/components/feedback/star-full.png')" class="feedback-small-stars" alt="star-full">
+                        <img v-if="stars<2" :src="require('@/assets/images/components/feedback/star-empty.png')" alt="star-empty"> 
+                        <img v-else :src="require('@/assets/images/components/feedback/star-full.png')" alt="star-full">
+                        <img v-if="stars<3" :src="require('@/assets/images/components/feedback/star-empty.png')" class="feedback-small-stars" alt="star-empty"> 
+                        <img v-else :src="require('@/assets/images/components/feedback/star-full.png')" class="feedback-small-stars" alt="star-full">
+                    </div>
+                    <div class="feedback-header-item "><h5 class="feedback-rounded-number">1</h5></div>
+                    <div class="feedback-header-item"><h5>CRACHÁ</h5></div>
+                </div>                
+            </template>
+            <br>
+            <div v-if="stars==0" class="feedback-content">
+                <img :src="require('@/assets/images/components/feedback/expression-star-0.png')" alt="expression-star-0">                 
+                <br>
+                <h5>QUE PENA!</h5>
+                <div class="feedback-itim"><h5>VOCÊ NÃO CONSEGUIU <span class="feedback-golden">NENHUMA ESTRELA</span>.</h5></div>
+                <div class="feedback-itim"><h5>TENTE NOVAMENTE!</h5></div>
+            </div> 
+            <div v-else-if="stars==1" class="feedback-content">
+                <img :src="require('@/assets/images/components/feedback/expression-star-1.png')" alt="expression-star-1">                 
+                <br>
+                <h5>QUASE!</h5>
+                <div class="feedback-itim"><h5>VOCÊ CONSEGUIU <span class="feedback-golden">UMA ESTRELA</span>.</h5></div>
+                <div class="feedback-itim"><h5>VAMOS TENTAR NOVAMENTE?</h5></div>
+            </div>
+            <div v-else-if="stars==2" class="feedback-content">
+                <img :src="require('@/assets/images/components/feedback/expression-star-2.png')" alt="expression-star-2">                 
+                <br>
+                <h5>MUITO BEM!</h5>
+                <div class="feedback-itim"><h5>VOCE CONSEGUIU <span class="feedback-golden">DUAS ESTRELAS</span>.</h5></div>
+                <div class="feedback-itim"><h5>DESEJA TENTAR NOVAMENTE?</h5></div>
+            </div>
+            <div v-else-if="stars==3" class="feedback-content">
+                <img :src="require('@/assets/images/components/feedback/expression-star-3.png')" alt="expression-star-3">                
+                <br>
+                <h5>PARABÉNS!</h5>
+                <div class="feedback-itim"><h5>VOCÊ COMPLETOU A ATIVIDADE</h5></div>
+                <div class="feedback-itim"><h5>COM <span class="feedback-golden">EXCELÊNCIA</span>!</h5></div>
+            </div>   
+            <br>
+            <div class="feedback-footer-buttons">
+                <img :src="require('@/assets/images/components/feedback/comecar/redo-button.png')" alt="redo-button"> 
+                <img :src="require('@/assets/images/components/feedback/comecar/next-button.png')" alt="next-button"> 
+            </div>  
+        </b-modal>    
+    </div> 
 </template>
 <script>
 import { mapState, mapActions } from 'vuex'
 export default {
     data(){
         return {
+            stars: 2,
             isVisible: false
         }
     },
@@ -26,7 +74,8 @@ export default {
     },
     watch: {
         isVisibleActivityAlertSuccess(value){
-            value ? this.$refs.modal.show() : this.$refs.modal.hide()
+            console.log(value)
+            this.isVisible = value
         }
     }
 }
