0f3b84140f37c07960844518fd465e138c844b8c
mario <m_s_r@outlook.com>
2019-04-15 12:22:33 -0300

    importando funcionalidades

--- src/renderer/components/ui/activities/MetadeLetras.vue
+++ src/renderer/components/ui/activities/MetadeLetras.vue
@@ -3,7 +3,13 @@
 </template>
 
 <script>
+import { ListMixin, CreateAnswersMixins } from './mixins'
+
 export default {
-    
+    mixins: [ListMixin, CreateAnswersMixins],
+    mounted(){
+        console.log(this.activity, 'atividade')
+        console.log(this.activity.items, 'items')
+    }
 }
 </script>
