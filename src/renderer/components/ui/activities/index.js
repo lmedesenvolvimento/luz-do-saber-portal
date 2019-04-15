950596fb44a75b72cb025902636c4e23803fc9bc
mario <m_s_r@outlook.com>
2019-04-15 11:23:00 -0300

    criando os arquivos da fase

--- src/renderer/components/ui/activities/index.js
+++ src/renderer/components/ui/activities/index.js
@@ -1,4 +1,5 @@
 
 export default {
-    'ls-activity-default': require('./Default.vue').default
+    'ls-activity-default': require('./Default.vue').default,
+    'ls-activity-half': require('./MetadeLetras.vue').default
 }
