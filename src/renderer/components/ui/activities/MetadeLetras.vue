b7d527be1f3b2aed9d004b0699af13e574497444
mario <m_s_r@outlook.com>
2019-04-16 12:10:46 -0300

    as letras já são escondidas seguindo as regras

--- src/renderer/components/ui/activities/MetadeLetras.vue
+++ src/renderer/components/ui/activities/MetadeLetras.vue
@@ -1,39 +1,43 @@
 <template>
     <div>
-        <p v-for="(item, index) in aux" :key="index">{{ item }}</p>
-        <p>{{ aux }}</p>
+        <div v-for="(item, index) in activity.items.keys" :key="index">
+            <p v-if="!item.hide">{{ item.text }}</p>
+            <p v-if="item.hide">escondido</p>
+        </div>
     </div>
 </template>
 
 <script>
-import { ListMixin, CreateAnswersMixins } from './mixins'
+import { ListMixin, MapMixins, CreateAnswersMixins } from './mixins'
+import { sampleSize, drop } from 'lodash'
 
 export default {
-    mixins: [ListMixin, CreateAnswersMixins],
-    data(){
-        return {
-            aux: []
-        }
-    },
+    mixins: [ListMixin, MapMixins, CreateAnswersMixins],
+    // data(){
+    //     return {
+    //     }
+    // },
     created(){
-        this.ActivityToArray(this.activity)
+        let size = (this.activity.items.keys.length % 2 === 0) ? this.activity.items.keys.length / 2 : Math.floor(this.activity.items.keys.length / 2) + 1
+        let aux = sampleSize(drop(this.activity.items.keys), size)
+
+        for (let i = 0; i < aux.length; i++){
+            for (let item in this.activity.items.keys){
+                if (aux[i] === this.activity.items.keys[item]){
+                    this.activity.items.keys[item].hide = true
+                }
+            }
+        }
     },
     mounted(){
-        
     },
-    methods: {
-        ActivityToArray(o){
-            console.log(o, 'atividade')
-            console.log(o.items, 'items')
-
-            let count = Object.keys(o.items.keys)
-
-            for (let i = 0; i < count.length; i++){
-                this.aux[i] = o.items.keys[i].text
-            }
-
-            console.log(this.aux, 'auxaaa')
-        }
-    },
+    // methods: {
+    // }
 }
 </script>
+
+<style>
+.omit{
+    display: none;
+}
+</style>
