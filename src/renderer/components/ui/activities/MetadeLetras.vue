f7bff8e2758b5c7751a912b52ece3c6dd33d1473
mario <m_s_r@outlook.com>
2019-04-22 11:03:52 -0300

    removing unnecessary code

--- src/renderer/components/ui/activities/MetadeLetras.vue
+++ src/renderer/components/ui/activities/MetadeLetras.vue
@@ -9,16 +9,13 @@
                 <ls-card-input-text
                     v-if="item.hide"
                     name="base-input" 
-                    :value="item" 
-                    :max-length="1" 
+                    :value="item"
                     type="value"
                 />
 
                 <ls-card-display v-if="!item.hide">{{ item.text }}</ls-card-display>
             </b-col>
         </b-row>
-
-        <p v-for="(item, id) in answers" :key="id">{{ item }}</p>
     </div>
 </template>
 
@@ -41,24 +38,24 @@ export default {
         //definindo quantidade de respostas. se par, 50% das letras somem, se impar 50% arredondado pra baixo + 1
         let size = (this.activity.items.keys.length % 2 === 0) ? this.activity.items.keys.length / 2 : Math.floor(this.activity.items.keys.length / 2) + 1
         // cria um array de respostas/letras que somem. drop usado por primeira letra nunca pode sumir
-        let aux = sampleSize(drop(this.activity.items.keys), size)
+        let keys = sampleSize(drop(this.activity.items.keys), size)
 
         // percorre todo o tamanho de respostas
-        range(0, aux.length).forEach(i => {
+        range(0, keys.length).forEach(i => {
             for (let item in this.activity.items.keys){
-                if (aux[i] === this.activity.items.keys[item])
+                if (keys[i] === this.activity.items.keys[item])
                     // esconde as letras selecionadas
                     this.activity.items.values[item].hide = true
             }
         })
 
         //guardando pra poder exibir como debug na tela 
-        this.answers = aux
+        this.answers = keys
 
         //seta as respostas
-        this.setAnswersArray(this.answers)
+        this.setAnswersArray(keys)
         // atualiza total de respostas válidas
-        this.setActivityAttrs({ total_correct_items: this.answers.length })
+        this.setActivityAttrs({ total_correct_items: keys.length })
     },
     mounted(){
     },
@@ -67,12 +64,11 @@ export default {
             let answers = []
 
             a.forEach(a => {
-                let aux = createAnswer(a, a.value_ids[0])
-                answers.push(aux)
+                let key = createAnswer(a, a.value_ids[0])
+                answers.push(key)
             })
 
             this.setAnswers(answers)
-            console.log('respostas', answers)
         },
         ...mapActions('Activity', ['setActivityAttrs'])
     }
