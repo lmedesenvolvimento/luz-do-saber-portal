b3328c64b817c338ab937c6fbb5335522bef6c34
Damis Garcia <damisgarcia@gmail.com>
2019-04-23 16:04:58 -0300

    add viewport

---
+++ src/renderer/components/ui/form/CardDroppable.vue
@@ -0,0 +1,33 @@
+<template>
+    <drop @drop="onDrop">
+        <div class="card-input card-droppable">
+            <b-card
+                no-body
+            >
+                <b-card-body>
+                    <slot name="img"></slot>
+                    <slot></slot>
+                </b-card-body>
+            </b-card>
+        </div>
+    </drop>
+</template>
+
+<script>
+import { Drop } from 'vue-drag-drop'
+import RadioInput from './RadioInput.vue'
+
+export default {
+    components: { Drop },
+    mixins: [RadioInput],
+    methods: {
+        onDrop(data, event){
+            console.log(data, event)
+        }
+    }
+}
+</script>
+
+<style>
+
+</style>
