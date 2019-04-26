788d989c7b69add0f1a8326327d9bd7271fe0f23
eduardo <eduregis1@gmail.com>
2019-04-26 09:13:21 -0300

    logica funcionando, arrays trocadas

--- src/renderer/components/ui/activities/UnscrambleName.vue
+++ src/renderer/components/ui/activities/UnscrambleName.vue
@@ -2,49 +2,62 @@
     <div class="container-fluid">
         <b-col v-if="hasKeys" class="activity-keys">
             <b-row align-v="center" align-h="center">
-                <b-col v-for="(item, position) in getKeys" :key="position" align-self="center" :sm="valueColSize" class="item"> 
-                    <Item 
+                <b-col v-for="(item, position) in getKeys" :key="position" align-self="center" :sm="valueColSize" class="item" @click="teste(item)"> 
+                    <ls-card-draggable 
                         v-if="answer.length"
                         :item="item"
-                        :type="'key'"
-                        :template="activity.item_template.key"
-                    />
+                        @click="console.log('a')"
+                    >
+                        {{ item.text }}
+                    </ls-card-draggable> 
                 </b-col>
             </b-row>            
-        </b-col>
-        <p>{{ progress }}</p>
-        <!-- <p v-for="(item, position) in getKeys" :key="position">{{ item.id }}</p> -->
+        </b-col> 
         <b-col class="activity-values">
             <b-row align-v="center" align-h="center">
                 <b-col v-for="(item, position) in getValues" :key="position" align-self="center" :sm="valueColSize" class="item" @click="checkLetterOrder()"> 
-                    <Item                         
+                    <ls-card-droppable                         
                         v-if="answer.length"
-                        :item="item"
-                        :type="'value'"
-                        :template="activity.item_template.value"                        
-                    />
+                        :item="item"                                               
+                    >
+                        {{ item.text }}
+                    </ls-card-droppable>
                 </b-col>                    
             </b-row>
-        </b-col>
+        </b-col>       
+        <p>{{ progress }}</p>                
+        <!-- <p v-for="(item, position) in getKeys" :key="position">{{ item.id }}</p> -->
+        
     </div>
 </template>
 <script>
+import { mapState, mapActions } from 'vuex'
+import ui from '@/components/ui'
+import alerts from '@/components/alerts'
 import { MapMixins, ListMixin, CreateAnswersMixins } from './mixins'
 
 export default {
+    components: { 
+        ...ui,
+        ...alerts
+    },
     mixins: [MapMixins, ListMixin, CreateAnswersMixins],
     data () {
         return {
             progress: 0
         }
     },
+    created(){
+        this.setActivityAttrs({ total_correct_items: 7 })
+    },
     mounted() {
         this.createAnswersArray()
     },
     methods: {
-        checkLetterOrder() {
-            
-        }
+        teste(a){
+            console.log(a)
+        },
+        ...mapActions('Activity', ['setActivityAttrs'])
     },    
 }
 </script>
