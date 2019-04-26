2b2877f31f22628ee54bf09f0b39975cfefb6009
João Victor de Alencar <joaovictoralencarpascoal@gmail.com>
2019-04-26 17:04:07 -0300

    align last box: left

--- src/renderer/components/pages/ThemePage.vue
+++ src/renderer/components/pages/ThemePage.vue
@@ -5,24 +5,27 @@
             :navbar-subtitle="'Unidades'"
             :navbar-icon="'https://placeimg.com/480/480/tech'"
         />
-        <div class="container">
-            <div class="py-5">
-                <b-col v-if="theme" class="my-5 theme-unities-list">
-                    <b-col v-for="unit in theme.units" :key="unit.id" class="mx-5 my-4 theme-unit">
-                        <router-link
-                            :to="{ 
-                                name: 'unit', 
-                                params: { 
-                                    module_slug: $route.params.module_slug, 
-                                    theme_slug: theme.slug,
-                                    unit_slug: unit.slug
-                                }
-                            }"
-                        >
-                            <themes-box :unit="unit" :theme-slug="theme.slug" />
-                        </router-link>
-                    </b-col>
-                </b-col>
+        <div class="mt-5 container">
+            <div class="mt-5 centralize">
+                <div class="mt-5">
+                    <b-list-group v-if="theme" horizontal class="mt-5 theme-unities-list">
+                        <b-list-group-item v-for="unit in theme.units" :key="unit.id" class="mx-5 my-4 flex-fill theme-unit">
+                            <router-link
+                                :to="{ 
+                                    name: 'unit', 
+                                    params: { 
+                                        module_slug: $route.params.module_slug, 
+                                        theme_slug: theme.slug,
+                                        unit_slug: unit.slug
+                                    }
+                                }"
+                            >
+                                <themes-box :unit="unit" :theme-slug="getThemeColor(theme.slug)" />
+                            </router-link>
+                        </b-list-group-item>
+                        <b-list-group-item v-show="emptyCellUnit" class="mx-5 my-4 flex-fill theme-unit" />
+                    </b-list-group>
+                </div>
             </div>
         </div>
     </div>
@@ -41,13 +44,43 @@ export default {
         Navbar, 
     },
     mixins: [RouteMixin],
+    data() {
+        return {
+            emptyCellUnit: false
+        }
+    },
     computed: {
-        ...mapState('Theme', ['theme'])
+        ...mapState('Theme', ['theme']),
+        
     },
     created(){
         this.fetchTheme(this.$route.params)
     },
+    updated() {
+        this.emptyCellUnit = this.alignEmptySpaces()
+    },
+
     methods: {
+        getThemeColor(theme){
+            switch (theme.slug) {
+            case 'comecar':
+                return  { color: '#C72929' }
+            case 'ler':
+                return { color: '#00963F' }
+            case 'escrever':
+                return { color: '#007CB2' }
+            default:
+                break;
+            }
+        },
+        alignEmptySpaces() {
+            var list = document.getElementsByClassName('theme-unities-list')[0]
+            var numUnities = list.getElementsByClassName('theme-unit').length
+
+            if (numUnities % 2 == 0){
+                return true
+            }
+        },
         ...mapActions('Theme', ['fetchTheme'])
     }
 };
