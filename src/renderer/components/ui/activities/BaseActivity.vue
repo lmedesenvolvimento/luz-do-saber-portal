2e8d4de36a31cd201928d31c4ca74cf17273bafc
eduardo <eduregis1@gmail.com>
2019-04-29 13:50:59 -0300

    adicionando atividade de quantidade de letras

--- src/renderer/components/ui/activities/BaseActivity.vue
+++ src/renderer/components/ui/activities/BaseActivity.vue
@@ -3,7 +3,7 @@
         <ls-activity-default v-if="subtypes.default === activity.subtype.slug" :value-col-size="valueColSize"></ls-activity-default>
         <ls-activity-half v-if="subtypes.hideHalf === activity.subtype.slug" :value-col-size="valueColSize"></ls-activity-half>
         <ls-activity-unscramble v-if="subtypes.unscramble === activity.subtype.slug" :value-col-size="valueColSize"></ls-activity-unscramble>
-        <ls-activity-number-letter v-if="subtypes.numberLetter === activity.subtype.slug" :value-col-size="valueColSize"></ls-activity-number-letter>
+        <ls-activity-number-letter v-if="subtypes.column === activity.subtype.slug" :value-col-size="valueColSize"></ls-activity-number-letter>
     </div>
 </template>
 <script>
