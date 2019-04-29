ca4b4f79c6f45d41474cbb9f201b33beee1d410e
mrodrigolira <liramailbox@gmail.com>
2019-04-29 14:52:21 -0300

    Creation of CardRadioInputCracha.vue was needed because cracha is a special type of radio input.

--- src/renderer/components/ui/items/custom/CrachaBox.vue
+++ src/renderer/components/ui/items/custom/CrachaBox.vue
@@ -1,5 +1,5 @@
 <template>
-    <ls-card-input 
+    <ls-card-input-cracha 
         class="col-sm" 
         label="item.text" 
         :type="type"
@@ -10,7 +10,7 @@
         <template slot="img">
             <b-card-img :src="require('@/assets/images/components/examples/cracha-1.png')" />
         </template>
-    </ls-card-input>
+    </ls-card-input-cracha>
 </template>
 <script>
 import { ItemProps } from '../index.js'
