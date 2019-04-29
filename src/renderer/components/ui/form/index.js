e5070fdc177dfc03a286a6424d4d884992d1889c
mrodrigolira <liramailbox@gmail.com>
2019-04-29 14:52:21 -0300

    Creation of CardRadioInputCracha.vue was needed because cracha is a special type of radio input.

--- src/renderer/components/ui/form/index.js
+++ src/renderer/components/ui/form/index.js
@@ -1,5 +1,6 @@
 export default {
     'ls-card-input': require('@/components/ui/form/CardRadioInput.vue').default,
+    'ls-card-input-cracha': require('@/components/ui/form/CardRadioInputCracha.vue').default,
     'ls-card-input-text': require('@/components/ui/form/CardInputKeydown.vue').default,
     'ls-card-display': require('@/components/ui/form/CardDisplay.vue').default,
     'ls-card-draggable': require('@/components/ui/form/CardDraggable.vue').default
