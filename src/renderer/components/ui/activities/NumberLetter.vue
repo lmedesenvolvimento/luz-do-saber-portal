b21cbdb2231ea77e1d9f923f64e1cf0da6ab9678
eduardo <eduregis1@gmail.com>
2019-04-30 10:27:06 -0300

    layout da atividade quantidade de letras

--- src/renderer/components/ui/activities/NumberLetter.vue
+++ src/renderer/components/ui/activities/NumberLetter.vue
@@ -15,22 +15,21 @@
         <b-row>
             <b-col>
                 <b-row align-v="center" align-h="center">
-                    <ls-card-display v-for="index in getKeys[0].text.length" :key="index" style="padding: 10px">
+                    <ls-card-display v-for="index in getKeys[0].text.length" :key="index" style="padding: 10px; margin: 130px 0 130px 0">
                         {{ getKeys[0].text[index - 1] }}
                     </ls-card-display>
                 </b-row>                
             </b-col>            
-        </b-row>        
+        </b-row> 
         <b-row>
             <b-col class="activity-values">
                 <b-row align-v="center" align-h="center">
-                    <b-col v-for="(item, position) in getValues" :key="position" align-self="center" :sm="valueColSize" class="item" style="padding-right: 110px"> 
+                    <b-col v-for="(item, position) in getValues" :key="position" align-self="center" :sm="valueColSize" class="item"> 
                         <Item 
                             v-if="answer.length"
                             :item="item"
                             :type="'value'"
                             :template="activity.item_template.value"
-                            style="width: 110px"
                         />
                     </b-col>                    
                 </b-row>
@@ -41,6 +40,7 @@
 <script>
 import { MapMixins, ListMixin, CreateAnswersMixins } from './mixins'
 import FormProps from '../form'
+import { range } from 'lodash'
 
 export default {
     components: {
