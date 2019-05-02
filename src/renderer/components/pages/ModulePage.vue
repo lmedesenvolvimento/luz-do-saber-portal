a496127affe5b8826c3f202ef31326ca6c9d2886
João Victor de Alencar <joaovictoralencarpascoal@gmail.com>
2019-05-02 11:46:48 -0300

    final version

--- src/renderer/components/pages/ModulePage.vue
+++ src/renderer/components/pages/ModulePage.vue
@@ -1,22 +1,24 @@
 <template>
-    <div id="module" class="page-container" :class="activeModule ? activeModule.slug : ''">
-        <div v-if="activeModule" class="container">
-            <navbar
-                :navbar-title="renderNavTitle"
-                :navbar-subtitle="'Temas'"
-                :navbar-icon="getModuleImage(activeModule)"
-            />
-            <b-row class="circle-list">
-                <b-col v-for="theme in activeModule.themes" :key="theme.id" cols="6" class="my-3 mx-4 circle-box">
-                    <router-link :to="{ name: 'theme', params: { module_slug: $route.params.module_slug, theme_slug: theme.slug } }">
-                        <vue-circle
-                            class=""
-                            :label="theme.title"
-                            :image="getThemeImage(theme.slug)"
-                            :progress="50"
-                            :color="getModuleColor(activeModule)"
-                        />
-                    </router-link>
+    <div id="module" v-if="activeModule" class="page-container" :class="activeModule ? activeModule.slug : ''">
+        <navbar
+            :navbar-title="renderNavTitle"
+            :navbar-subtitle="'Temas'"
+            :navbar-icon="getModuleImage(activeModule)"
+        />
+        <div class="page-container-wrap-spacing">
+            <b-row>
+                <b-col v-for="theme in activeModule.themes" :key="theme.id" cols="12" md="6">
+                    <div class="circle-box">
+                        <router-link :to="{ name: 'theme', params: { module_slug: $route.params.module_slug, theme_slug: theme.slug } }">
+                            <vue-circle
+                                class=""
+                                :label="theme.title"
+                                :image="getThemeImage(theme.slug)"
+                                :progress="50"
+                                :color="getModuleColor(activeModule)"
+                            />
+                        </router-link>
+                    </div>
                 </b-col>
                 <b-col cols="12">
                     <b-btn variant="link" :to="historyBack">
