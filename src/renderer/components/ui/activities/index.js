93c6c9061a8452e799f1ca3091b7dbe16b401695
davi <manelv8@gmail.com>
2019-05-02 12:31:05 -0300

    remove unused code

--- src/renderer/components/ui/activities/index.js
+++ src/renderer/components/ui/activities/index.js
@@ -3,5 +3,4 @@ export default {
     'ls-activity-default': require('./Default.vue').default,
     'ls-activity-half': require('./HideHalf.vue').default,
     'ls-activity-unscramble': require('./UnscrambleName.vue').default,
-    'ls-activity-scratch-letter': require('./ScratchLetter.vue').default
 }
