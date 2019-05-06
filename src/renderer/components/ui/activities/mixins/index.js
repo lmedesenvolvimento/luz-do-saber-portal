a2f540c15efc7ff353bd6a27155c8976dac847cf
mario <m_s_r@outlook.com>
2019-05-06 11:32:24 -0300

    adding defined colors

--- src/renderer/components/ui/activities/mixins/index.js
+++ src/renderer/components/ui/activities/mixins/index.js
@@ -26,6 +26,26 @@ export const ListMixin = {
         },
         getValues() {
             return this.activity.items.values
+        },
+        getColorsArray() {
+            let c = ['cc7200', 'c30374', '8c2db4', '454ebc', '3875c1', '028d8b', 'ffb151', 'f766ae', 'ae67e0', '757bfa', '75c0ff', '64cbc2']
+            let r = []
+            let aux = 0
+
+            for (let i = 0; i < 100; i++) {
+                if (aux < c.length - 1) {
+                    //r.push(c[aux])
+                    aux++
+                }
+                else {
+                    aux = 0
+                    //r.push(c[aux])
+                }
+
+                r.push(c[aux])
+            }
+
+            return r
         }
     }
 }
