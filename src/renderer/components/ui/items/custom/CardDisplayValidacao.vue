83ca6bc745026a8ca64e3495cd9c92fd422038db
davi <manelv8@gmail.com>
2019-05-06 18:26:07 -0300

    show correct letter

--- src/renderer/components/ui/items/custom/CardDisplayValidacao.vue
+++ src/renderer/components/ui/items/custom/CardDisplayValidacao.vue
@@ -5,23 +5,35 @@
         :type="type"
         :item="item"
     >
-        {{ answers ? 'answer': 'no answer' }} 
+        {{ letter }} 
     </ls-card-display>
 </template>
 <script>
 import { find, values } from 'lodash'
 import { ItemProps } from '../index.js'
+import Activity from '../../../../store/modules/Activity';
 
 export default {
     mixins: [ItemProps],
-    created(){
-        console.log('created')
-        console.log('item', this.item)
+    data() {
+        return {
+            letter: '__'
+        }
     },
     watch: {
         answers(data){
-            let res = find(values(this.answers), (value) => value.key.data === this.item.id )
+            let res = find(values(this.answers), (value) => value.key.data === this.item.id && value.valid === true)
+
+            if( res ){
+                
+                let l = find(this.activity.items.keys, (value => { return value.id === res.key.data }))
+                console.log('letter', l.text)
+                console.log('res key data', res.key.data)
+                this.letter = l.text
+            
+            }
         }
     }
+
 }
 </script>
