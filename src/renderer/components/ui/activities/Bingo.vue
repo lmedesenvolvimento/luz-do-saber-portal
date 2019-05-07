c27fa2c54dd1af8d73a1805cff8176b7c9a71d7c
eduardo <eduregis1@gmail.com>
2019-05-07 11:35:53 -0300

    adicionando letras do bingo

--- src/renderer/components/ui/activities/Bingo.vue
+++ src/renderer/components/ui/activities/Bingo.vue
@@ -1,20 +1,37 @@
 <template>
     <div class="container-fluid">
-        <b-col cols="9" align-v="center" align-h="center">
-            <b-row v-for="item in getValues" :key="item" :sm="valueColSize">
-                <ls-card-display>
-                    <b-row align-v="center" align-h="center"> 
-                        <b-col>
-                            <b-row style="padding: 0 20px 0 10px">
-                                <ls-card-input v-for="letter in item.text" :key="letter.id" style="margin-left: 10px">
-                                    {{ letter }}
-                                </ls-card-input> 
-                            </b-row>                            
-                        </b-col> 
-                    </b-row>
-                </ls-card-display>
-            </b-row>
-        </b-col>
+        <b-row>
+            <b-col align-v="center" align-h="center">
+                <div>                   
+                    <div v-for="bingoLetter in alphabet" :key="bingoLetter" class="bingoLetter">
+                        {{ bingoLetter }}
+                    </div>
+                </div>
+            </b-col>
+            <b-col align-v="center" align-h="center">
+                <b-row v-for="item in getValues" :key="item" :sm="valueColSize">
+                    <ls-card-display>
+                        <b-row v-if="item === getValues[0]" align-v="center" align-h="center" style="margin: 10px">
+                            <p>sua cartela</p>
+                        </b-row>
+                        <b-row> 
+                            <b-col>
+                                <b-row v-if="item === getValues[0]" class="bingoCard" align-v="center" align-h="center">
+                                    <ls-card-input v-for="letter in item.text" :key="letter.id" style="margin-left: 10px">
+                                        {{ letter }}
+                                    </ls-card-input>                                    
+                                </b-row>
+                                <b-row v-else class="bingoCard" align-v="center" align-h="center">
+                                    <ls-card-display v-for="letter in item.text" :key="letter.id" style="margin-left: 10px">
+                                        {{ letter }}
+                                    </ls-card-display>                                    
+                                </b-row>                       
+                            </b-col> 
+                        </b-row>
+                    </ls-card-display>
+                </b-row>
+            </b-col>
+        </b-row>
     </div>
 </template>
 <script>
@@ -24,12 +41,17 @@ import alerts from '@/components/alerts'
 import { sortBy, shuffle } from 'lodash'
 import { MapMixins, ListMixin, CreateAnswersMixins } from './mixins'
 
-export default {
+export default {    
     components: { 
         ...ui,
         ...alerts
     },
-    mixins: [MapMixins, ListMixin, CreateAnswersMixins],    
+    mixins: [MapMixins, ListMixin, CreateAnswersMixins], 
+    data() {
+        return {
+            alphabet: ['a','b','c','d','e','f','g','h','i','j','k','l','m','n','o','p','q','r','s','t','u','v','w','x','y','z']
+        }
+    },   
     mounted() {
         this.createAnswersArray()
     },
@@ -40,9 +62,20 @@ export default {
 </script>
 
 <style>
-    .aaaaa{
-        border: solid 1px;
-        display: inline;
+    .bingoCard{
+        width: 600px;
+        padding: 0 10px 0 10px;
+    }
+    .bingoLetter{
+        display: inline-grid;
+        text-align: center;
+        padding-top: 3px;
+        color: white;
+        background-color: #ccc;
+        border-radius: 13px;
+        width: 25px;
+        height: 25px;
+        margin: 2px;
     }
 </style>
 
