ac78e335bf1e5276116905837703844b2f3b2a24
eduardo <eduregis1@gmail.com>
2019-05-08 09:49:53 -0300

    ajustando cores do layout

--- src/renderer/components/ui/activities/Bingo.vue
+++ src/renderer/components/ui/activities/Bingo.vue
@@ -1,28 +1,29 @@
 <template>
-    <div class="container-fluid">
-        <b-row>
-            <b-col align-v="center" align-h="center">
+    <b-container>
+        <br><br><br><br>
+        <b-row align-v="start">
+            <b-col cols="3" align-v="center" align-h="center">
                 <div>                   
-                    <div v-for="bingoLetter in alphabet" :key="bingoLetter" class="bingoLetter">
+                    <div v-for="bingoLetter in alphabet" :key="bingoLetter" :class="{bingoLetter, bingoRaffleLetter: searchString(raffleLetters,bingoLetter)}" @click="raffle(bingoLetter)">
                         {{ bingoLetter }}
                     </div>
-                </div>
+                </div>                
             </b-col>
-            <b-col align-v="center" align-h="center">
+            <b-col cols="9" align-v="center" align-h="center">
                 <b-row v-for="item in getValues" :key="item" :sm="valueColSize" :class="{bingoCardPlayer: item === getValues[0]}">
-                    <ls-card-display>
+                    <ls-card-display v-if="item !== getValues[3]">
                         <b-row v-if="item === getValues[0]" align-v="center" align-h="center" style="margin: 10px">
                             <p>sua cartela</p>
                         </b-row>
                         <b-row> 
                             <b-col>
                                 <b-row v-if="item === getValues[0]" class="bingoCardLetter" align-v="center" align-h="center">
-                                    <ls-card-input v-for="letter in item.text" :key="letter.id" style="margin-left: 10px">
+                                    <ls-card-input v-for="letter in item.text" :key="letter" style="margin-left: 10px">
                                         {{ letter }}
                                     </ls-card-input>                                    
                                 </b-row>
                                 <b-row v-else class="bingoCardLetter" align-v="center" align-h="center">
-                                    <ls-card-display v-for="letter in item.text" :key="letter.id" style="margin-left: 10px">
+                                    <ls-card-display v-for="letter in item.text" :key="letter" style="margin-left: 10px">
                                         {{ letter }}
                                     </ls-card-display>                                    
                                 </b-row>                       
@@ -32,7 +33,7 @@
                 </b-row>
             </b-col>
         </b-row>
-    </div>
+    </b-container>
 </template>
 <script>
 import { mapState, mapActions } from 'vuex'
@@ -49,13 +50,23 @@ export default {
     mixins: [MapMixins, ListMixin, CreateAnswersMixins], 
     data() {
         return {
-            alphabet: ['a','b','c','d','e','f','g','h','i','j','k','l','m','n','o','p','q','r','s','t','u','v','w','x','y','z']
+            alphabet: ['a','b','c','d','e','f','g','h','i','j','k','l','m','n','o','p','q','r','s','t','u','v','w','x','y','z'],
+            raffleLetters: []
         }
     },   
     mounted() {
         this.createAnswersArray()
     },
-    methods: {        
+    methods: {       
+        raffle(letter) {
+            this.raffleLetters.push(letter);
+        },
+        searchString(arr, str) {
+            for(let i = 0; i < arr.length;i++){
+                if (arr[i].match(str)) return true;
+            }
+            return false;
+        },
         ...mapActions('Activity', ['setActivityAttrs'])
     },    
 }
@@ -63,7 +74,7 @@ export default {
 
 <style>
     .bingoCardPlayer .card-body{
-        background-color: orange;
+        background-color: #ffb141;
     }
     .bingoCardLetter{
         width: 600px;
@@ -72,13 +83,16 @@ export default {
     .bingoLetter{
         display: inline-grid;
         text-align: center;
-        padding-top: 3px;
+        padding-top: 5px;
         color: white;
-        background-color: #ccc;
-        border-radius: 13px;
-        width: 25px;
-        height: 25px;
+        background-color: #dfd9db;
+        border-radius: 15px;
+        width: 30px;
+        height: 30px;
         margin: 2px;
+    }
+    .bingoRaffleLetter{
+        background-color: #13c5c4;
     }
     .bingoCardLetter .card-body{
         background-color: white;
