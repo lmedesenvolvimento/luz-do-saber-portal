2dddde80dbb3d678c1a458c55edf5b7427b44c56
Damis Garcia <damisgarcia@gmail.com>
2019-05-15 16:31:35 -0300

    Focus in first input[text], auto focus next

--- src/renderer/directives/index.js
+++ src/renderer/directives/index.js
@@ -1,9 +1,7 @@
 import Vue from 'vue'
 
 Vue.directive('focus', {
-    // Quando o elemento vinculado é inserido no DOM...
     inserted: function (el) {
-        // Coloque o foco no elemento
         el.focus()
     }
 })
