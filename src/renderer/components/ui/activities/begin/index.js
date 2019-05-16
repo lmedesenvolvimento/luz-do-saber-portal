0a84dae6aca45e3a1e543730dfcaab714ae2c82a
Damis Garcia <damisgarcia@gmail.com>
2019-05-16 17:06:27 -0300

    Rearrange folders

---
+++ src/renderer/components/ui/activities/begin/index.js
@@ -0,0 +1,7 @@
+export default {
+    'ls-activity-begin-number-letter': require('@ui/activities/begin/NumberLetter.vue').default,
+    'ls-activity-begin-half': require('@ui/activities/begin/games/HideHalf.vue').default,
+    'ls-activity-begin-unscramble': require('@ui/activities/begin/games/UnscrambleName.vue').default,
+    'ls-activity-begin-jigsaw': require('@ui/activities/begin/games/Jigsaw.vue').default,
+    'ls-activity-begin-rearrange-name': require('@ui/activities/begin/games/RearrangeName.vue').default,
+}
