0fb24bc978d63fa1747ef5e433fa19c408d85b6b
João Victor de Alencar <joaovictoralencarpascoal@gmail.com>
2019-05-21 09:58:03 -0300

    dragging syllables

--- src/renderer/components/ui/activities/DraggingSyllables.vue
+++ src/renderer/components/ui/activities/DraggingSyllables.vue
@@ -1,61 +1,49 @@
 <template>
-    <div class="container-fluid">
-        <b-row>
-            <b-col cols="6" md="6">
-                <b-col
-                    v-for="item in activity.items.values" 
-                    :key="item.id"
-                    :sm="valueColSize"
-                    cols="12"
-                    md="4"
-                >
-                    {{ item.text }}
-                </b-col>
+    <div>
+        <b-row align-h="center">
+            <b-col cols="12" md="4">
+                <b-row>
+                    <b-col
+                        v-for="item in activity.items.values" 
+                        :key="item.id"
+                        cols="12"
+                        md="6"
+                    >
+                        <ls-card-draggable :item="item">
+                            {{ item.text }}
+                        </ls-card-draggable>
+                    </b-col>
+                </b-row>              
             </b-col>
-            <b-col cols="6" md="6">
+            <b-col cols="12" md="8">
                 <b-row
                     v-for="item in activity.items.keys"
                     :key="item.id"
+                    align-v="center"
                 >
-                    <b-col v-if="item.images[0].url !== null" class="key-image" :style="{ 'background-image': 'url(' + item.images[0].url + ')' }">
-                        {{ item.images[0].url }}
+                    <b-col cols="12" md="4">
+                        <ls-card-display v-if="item.images[0].url !== null" class="key-image" :style="{ 'background-image': 'url(' + item.images[0].url + ')' }">
+                            imagem 
+                            <br>
+                            aqui
+                        </ls-card-display>
                     </b-col>
-                    <b-col>
-                        <b-col 
-                            v-for="valueId in item['value_ids']"
-                            :key="valueId">
-                            {{ getSyllableByValueId(valueId) }}
-                        </b-col>
-                        <br>
-                    </b-col>
-                </b-row>
-            </b-col>
-            <!-- <b-row align-h="center" align-v="center">
-                <b-col class="activity-keys">
-                    <b-row align-h="center" align-v="center">
-                        <b-col
-                            v-for="item in activity.items.values" 
-                            :key="item.id"
-                            :sm="valueColSize"
-                            class="item"
-                        >
-                            <Item
-                                v-if="item.hide"
+                    <b-col cols="12" md="8">
+                        <b-row>
+                            <ls-card-droppable
+                                v-for="valueId in item['value_ids']"
+                                :key="valueId"
+                                class="drop"
                                 :item="item"
                                 :type="'value'"
                                 :template="activity.item_template.value"
-                            />
-
-                            <Item
-                                v-if="!item.hide"
-                                :item="item"
-                                :type="'key'"
-                                :template="activity.item_template.key"
-                            />
-                        </b-col>
-                    </b-row>
-                </b-col>
-            </b-row> -->
+                            >
+                                {{ getSyllableByValueId(valueId) }}
+                            </ls-card-droppable>
+                        </b-row>
+                    </b-col>
+                </b-row>
+            </b-col>
         </b-row>
     </div>
 </template>
@@ -63,6 +51,7 @@
 import { MapMixins, ListMixin, CreateAnswersMixins } from './mixins'
 import FormProps from '../form'
 import { range } from 'lodash'
+
 
 export default {
     components: {
@@ -74,21 +63,37 @@ export default {
         }
     },
     mixins: [MapMixins, ListMixin, CreateAnswersMixins],
-    // computed: {
-    //     invertedArray(array) {
-    //         return array.reverse()
-    //     }
-    // },
     mounted() {
         this.createAnswersArray()
+        this.teste()
     },
     methods: {
         getSyllableByValueId(valueId) {
             for (let i = 0; i < this.activity.items.values.length; i++) {
                 if (valueId == this.activity.items.values[i].id){
+                    console.log('Sílaba: '+ this.activity.items.values[i].text)
                     return this.activity.items.values[i].text;
                 }
             }
+        },
+        teste(val){
+            let a = JSON.parse(JSON.stringify(this.activity.items.values));
+
+            for (let i = 0; i < a.length; i++){
+                a[i].text = this.activity.items.values[i].text
+            }
+
+            for( let i = 0; i < this.activity.items.keys.length; i++){
+                console.log('entrei i '+ i)
+                for(let j = 0; j < this.activity.items.keys[i].value_ids.length; j++){
+                    console.log('entrei j '+j)
+                    //if (this.activity.items.keys[i].value_ids.includes(a[i].key_id)){
+                    //    console.log(a[i].text)
+                    //}
+                    this.getSyllableByValueId(this.activity.items.keys[i].value_ids[j])
+                }  
+            }
+            console.log(a);
         }
     },
 }
