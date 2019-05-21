bb691f344da4fe011b8582e0a39db6913642631b
mario <m_s_r@outlook.com>
2019-05-21 11:53:08 -0300

    adicionando o componente de multi drop

--- src/renderer/constants/index.js
+++ src/renderer/constants/index.js
@@ -22,6 +22,7 @@ export const GameSubtypes = {
     unscramble: 'desembaralhar-nome',
     rearrangeName: 'monte-seu-nome',
     jigsaw: 'quebra-cabecas',
+    boxWords: 'caixa-de-palavras',
 }
 
 export const ItemTypes = {
