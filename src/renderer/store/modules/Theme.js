9476bb8518dbeec7c0636388c77d06855829346c
Damis Garcia <damisgarcia@gmail.com>
2019-05-22 11:30:46 -0300

    resolve cache pages

--- src/renderer/store/modules/Theme.js
+++ src/renderer/store/modules/Theme.js
@@ -14,6 +14,10 @@ const actions = {
     async fetchTheme({ commit }, params){
         let { data } = await API.get(`/game/${params.module_slug}/${params.theme_slug}`)
         commit('SET_THEME',data)
+    },
+
+    destroyTheme({ commit }){
+        commit('SET_THEME', {})
     }
 }
 
