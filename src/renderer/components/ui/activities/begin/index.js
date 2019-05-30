535e700f90a8522f0a97173d2dcf0dfab57d7026
João Victor de Alencar <joaovictoralencarpascoal@gmail.com>
2019-05-30 11:33:59 -0300

    organizing folders

--- src/renderer/components/ui/activities/begin/index.js
+++ src/renderer/components/ui/activities/begin/index.js
@@ -8,4 +8,5 @@ export default {
     'ls-activity-begin-domino-name': require('@ui/activities/begin/games/DominoName').default,
     'ls-activity-begin-bingo': require('@ui/activities/begin/games/Bingo.vue').default,
     'ls-activity-friends-name': require('@ui/activities/begin/games/FriendsName.vue').default,
+    'ls-activity-match-friends-initial': require('@ui/activities/begin/games/MatchFriendsInitial.vue').default,
 }
