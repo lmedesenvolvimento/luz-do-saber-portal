33eb43e553d797eaf4581d70b74adfbda33a0035
eduardo <eduregis1@gmail.com>
2019-06-14 10:39:46 -0300

    cards de input e display

--- src/renderer/components/ui/activities/read/games/SecretWord.vue
+++ src/renderer/components/ui/activities/read/games/SecretWord.vue
@@ -1,12 +1,61 @@
 <template>
     <div class="container-fluid">
-        aaaa
+        <b-row align-h="center">
+            <b-col>
+                <img 
+                    :src="getKeys[0].images[0].url" 
+                    alt=""
+                >
+            </b-col>
+            <b-col
+                v-for="(letter, index) in getKeys[0].text" 
+                :key="index"
+                :sm="1"
+                class="item"
+            >
+                <div class="letra">
+                    <ls-card-display>
+                        {{ letter }}
+                    </ls-card-display>
+                </div>
+            </b-col>  
+        </b-row>
+        <b-row
+            sm="1"
+        >
+            <b-col
+                v-for="(letter, position) in alphabet"
+                :key="position"  
+                :sm="1"
+                class="item"
+            >
+                <div class="letra">
+                    <ls-card-input>
+                        {{ letter }}
+                    </ls-card-input>
+                </div> 
+            </b-col>
+        </b-row>        
     </div>
 </template>
 
 <script>
+import { ListMixin, MapMixins, CreateAnswersMixins, createAnswer } from '@ui/activities/mixins'
+import ui from '@/components/ui'
 export default {
-    
+    components: { 
+        ...ui
+    },
+    mixins: [MapMixins, ListMixin, CreateAnswersMixins],
+    data () {
+        return {
+            alphabet: ['A','B','C','D','E','F','G','H','I','J','K','L','M','N','O','P','Q','R','S','T','U','V','W','X','Y','Z']
+        }
+    },
+    mounted() {
+        this.createAnswersArray()
+        this.addColorsToType('substantivo_comum')
+    },    
 }
 </script>
 
