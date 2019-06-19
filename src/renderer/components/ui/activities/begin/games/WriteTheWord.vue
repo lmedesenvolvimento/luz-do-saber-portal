f51efc3bbd3f1d086e00f43ddcb289a16be4b9cc
rodrigoronin <liramailbox@gmail.com>
2019-06-19 18:01:44 -0300

    Added the backbone of the activity. Modified the navbar.vue to hide the list icon when in Modules pages.
    Former-commit-id: 57549a7a76910c45b54f702a9c5aa095219970ea

--- src/renderer/components/ui/activities/begin/games/WriteTheWord.vue
+++ src/renderer/components/ui/activities/begin/games/WriteTheWord.vue
@@ -1,10 +1,56 @@
 <template>
     <div class="container-fluid">
         <b-row>
-            <b-col>
-                <h1>TESTE</h1>
-                <ls-card-audio-listen></ls-card-audio-listen>
+            <b-col v-if="hasKeys" class="activity-keys">
+                <b-row>
+                    <b-col v-for="(item, position) in getKeys" :key="position" :sm="keyColSize" class="item"> 
+                        <Item 
+                            v-if="answers"
+                            :item="item"
+                            :type="'key'"
+                            :template="activity.item_template.key"
+                        />
+                        <ls-card-audio-listen
+                            :item="item"
+                        >
+                            Ronaldo
+                        </ls-card-audio-listen>
+                    </b-col>
+                </b-row>
+            </b-col>
+        </b-row>
+        <b-row>
+            <b-col 
+                :class="{ 
+                    'flex-4': getValues.length >= 12 
+                }"
+                class="activity-values" 
+            >
+                <b-row>
+                    <b-col v-for="(item, position) in getValues" :key="position" align-self="center" :md="valueColSize" :sm="6" class="item"> 
+                        <Item 
+                            v-if="answers"
+                            :item="item"
+                            :type="'value'"
+                            :template="activity.item_template.value"
+                        />
+                    </b-col>                    
+                </b-row>
             </b-col>
         </b-row>
     </div>
 </template>
+<script>
+import { ListMixin, MapMixins, CreateAnswersMixins, createAnswer } from '@ui/activities/mixins'
+import ItemComponents from '@ui/form/index.js'
+
+export default {
+    components: {
+        ...ItemComponents,
+    },
+    mixins: [ListMixin, MapMixins, CreateAnswersMixins],
+    mounted() {
+        console.log(this.item);
+    }
+}
+</script>
