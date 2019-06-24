5efd676ee6fe49d22dd153c99ba0fbb43722d9b5
Nathan Souza <ant.nathan15@gmail.com>
2019-06-24 09:23:19 -0300

    implemented letter group
    fonts still missing
    
    
    Former-commit-id: aab7ae43c060b15a51f735ffd9cfb5f83866c0be

--- src/renderer/components/ui/activities/read/index.js
+++ src/renderer/components/ui/activities/read/index.js
@@ -1,3 +1,4 @@
 export default{
     'ls-activity-read-grouping': require('@ui/activities/read/games/Grouping.vue').default,
+    'ls-activity-read-letter-group': require('@ui/activities/read/games/LetterGroup.vue').default,
 }
