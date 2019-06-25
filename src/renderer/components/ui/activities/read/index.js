d06b841d311db658834d7bf8b394f0d9cb1746e7
lord <m_s_r@outlook.com>
2019-06-25 09:53:30 -0300

    caixas do tamanho e posicionamento corretos
    Former-commit-id: 727e52bc6a9e67c97ee3c5ddc1fe916192470556

--- src/renderer/components/ui/activities/read/index.js
+++ src/renderer/components/ui/activities/read/index.js
@@ -1,4 +1,4 @@
 export default{
-    'ls-activity-grouping': require('@ui/activities/read/games/Grouping.vue').default,
+    'ls-activity-read-grouping': require('@ui/activities/read/games/Grouping.vue').default,
     'ls-activity-read-dragging-syllables': require('@ui/activities/read/games/DraggingSyllables.vue').default,
 }
