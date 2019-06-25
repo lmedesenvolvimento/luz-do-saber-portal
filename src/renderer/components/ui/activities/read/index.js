96376f2b3af9e0117202c959a271b48383b00357
eduardo <eduregis1@gmail.com>
2019-06-25 11:04:03 -0300

    ajustando o endereçamento da questão
    Former-commit-id: 58c9aee2734ab68b1937a11e2958825e37c54fe1

--- src/renderer/components/ui/activities/read/index.js
+++ src/renderer/components/ui/activities/read/index.js
@@ -1,4 +1,5 @@
 export default{
     'ls-activity-grouping': require('@ui/activities/read/games/Grouping.vue').default,
     'ls-activity-read-dragging-syllables': require('@ui/activities/read/games/DraggingSyllables.vue').default,
+    'ls-activity-read-secret-word': require('@ui/activities/read/games/SecretWord.vue').default
 }
