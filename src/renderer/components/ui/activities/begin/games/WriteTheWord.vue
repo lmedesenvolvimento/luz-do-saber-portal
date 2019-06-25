1170048b1776a3cb6798decfdd2eb0772cb71494
rodrigoronin <liramailbox@gmail.com>
2019-06-25 14:22:25 -0300

    Layout done. Feature to show the wrong word on the right panel almost done.
    Former-commit-id: 5a0a7801642263a2f493057f6476bdacd6cc45d2

--- src/renderer/components/ui/activities/begin/games/WriteTheWord.vue
+++ src/renderer/components/ui/activities/begin/games/WriteTheWord.vue
@@ -1,68 +1,64 @@
 <template>
     <div class="container-fluid">
-        <b-col>
-            <b-row>
-                <b-col v-if="hasKeys" class="activity-keys">
-                    <b-row>
-                        <b-col v-for="(item, position) in getKeys" :key="position" :sm="keyColSize" class="item"> 
-                            <!-- <Item 
-                                v-if="answers"
-                                :item="item"
-                                :type="'key'"
-                                :template="activity.item_template.key"
-                            /> -->
-                            <ls-card-audio-listen
-                                :item="item"
-                                :type="key"
-                                :template="activity.item_template.key"
-                            >
-                                <!-- <img src="https://picsum.photos/300/200" alt=""> -->
-                                <!-- {{ item.images[0].url }} -->
-                            </ls-card-audio-listen>
-                        </b-col>
-                    </b-row>
-                </b-col>
-            </b-row>
+        <b-row class="write-word">
+            <b-col class="col-sm-4">
+                <b-row v-if="hasKeys" class="activity-keys">
+                    <b-col v-for="(item, position) in getKeys" :key="position" :sm="keyColSize" class="item"> 
+                        <!-- <Item 
+                            v-if="answers"
+                            :item="item"
+                            :type="'key'"
+                            :template="activity.item_template.key"
+                        /> -->
+                        <ls-card-audio-listen
+                            :item="item"
+                            :template="activity.item_template.key"
+                        >
+                            <img src="https://picsum.photos/300/200" alt="">
+                            <!-- {{ item.letters[0].text }} -->
+                        </ls-card-audio-listen>
+                    </b-col>
+                
+                    <b-col 
+                        :class="{ 
+                            'flex-4': getValues.length >= 12 
+                        }"
+                        class="activity-values" 
+                    >
+                        <div class="card-input card-input-text mt-2">
+                            <label>
+                                <b-card 
+                                    no-body
+                                >
+                                    <b-card-body>
+                                        <input
+                                            v-model="answer"
+                                            v-focus="true"
+                                            type="text"
+                                            maxlength="10"
+                                            autocomplete="off"
+                                            required
+                                        />
+                                    </b-card-body>
+                                </b-card>
+                            </label>
+                        </div>
+                    </b-col>
+                </b-row>
+            </b-col>
 
-            <b-row>
-                <b-col 
-                    :class="{ 
-                        'flex-4': getValues.length >= 12 
-                    }"
-                    class="activity-values" 
-                >
-                    <div class="card-input card-input-text mt-2">
-                        <label>
-                            <b-card 
-                                no-body
-                            >
-                                <b-card-body>
-                                    <input
-                                        v-focus="true"
-                                        type="text"
-                                        maxlength="8"
-                                        autocomplete="off"
-                                        required
-                                    />
-                                </b-card-body>
-                            </b-card>
-                        </label>
+            <b-col class="col-sm-4">
+                <ls-card-display class="wrong-words-display">
+                    <p>palavras erradas:</p>
+
+                    <div class="wrong-words-list">
+                        <span>{{ answer }}</span>
+                        <span>segunda</span>
+                        <span>terceira</span>
                     </div>
-                </b-col>
-            </b-row>
-        </b-col>
-
-        <b-col>
-            <ls-card-display>
-                <p>palavras erradas:</p>
-
-                <ul>
-                    <li>Primeira</li>
-                    <li>Palavra</li>
-                    <li>Errada</li>
-                </ul>
-            </ls-card-display>
-        </b-col>
+                </ls-card-display>
+            </b-col>
+        </b-row>
     </div>
 </template>
 <script>
@@ -74,5 +70,14 @@ export default {
         ...ItemComponents,
     },
     mixins: [ListMixin, MapMixins, CreateAnswersMixins],
+    data() {
+        return {
+            answer: '',
+        }
+    },
+    mounted() {
+        console.log(this.item);
+        
+    }
 }
 </script>
