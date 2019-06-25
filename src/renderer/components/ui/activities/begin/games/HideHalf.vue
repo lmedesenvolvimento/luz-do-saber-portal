c2895496501c78108cc08b58edecbc8baa9b2387
lord <m_s_r@outlook.com>
2019-06-25 11:47:43 -0300

    acomodando mais de uma questão
    Former-commit-id: 5a46bc6ddc28f6b7b05631df1921037afb4911ab

--- src/renderer/components/ui/activities/begin/games/HideHalf.vue
+++ src/renderer/components/ui/activities/begin/games/HideHalf.vue
@@ -10,14 +10,14 @@
                         class="item"
                     >
                         <Item
-                            v-if="item.hide"
+                            v-if="notImage && item.hide"
                             :item="item"
                             :type="'value'"
                             :focus="item.focus"
                             :template="activity.item_template.value"
                         />
                         <Item
-                            v-if="!item.hide"
+                            v-if="notImage && !item.hide"
                             :item="item"
                             :type="'key'"
                             :template="activity.item_template.key"
@@ -40,27 +40,14 @@ import { mapActions } from 'vuex'
 export default {
     components: { Item },
     mixins: [ListMixin, MapMixins, CreateAnswersMixins],
+    computed: {
+        notImage(){
+            return this.activity.item_template.key.type !== 'imagem'
+        }
+    },
     created(){
-        //definindo quantidade de respostas. se par, 50% das letras somem, se impar 50% arredondado pra baixo + 1
-        let size = (this.activity.items.keys.length % 2 === 0) ? this.activity.items.keys.length / 2 : Math.floor(this.activity.items.keys.length / 2) + 1
-        // cria um array de respostas/letras que somem. drop usado por primeira letra nunca pode sumir
-        let keys = sampleSize(drop(this.activity.items.keys), size)
-
-        // percorre todo o tamanho de respostas
-        range(0, keys.length).forEach(i => {
-            for (let item in this.activity.items.keys){
-                if (keys[i] === this.activity.items.keys[item])
-                    // esconde as letras selecionadas
-                    this.activity.items.values[item].hide = true
-            }
-        })
-
-        let values = filter(this.activity.items.values, v => v.hide)
-
-        values[0].focus = true
-        //seta as respostas
-        this.setAnswersArray(keys)
-        this.setActivityAttrs({ total_correct_items: keys.length })
+        if (this.notImage) this.dropLetters()
+        else console.log('owo')
     },
     methods: {
         setAnswersArray(a){
@@ -73,7 +60,33 @@ export default {
 
             this.setAnswers(answers)
         },
-        ...mapActions('Activity', ['setActivityAttrs'])
+        dropLetters(){
+            //definindo quantidade de respostas. se par, 50% das letras somem, se impar 50% arredondado pra baixo + 1
+            let size = (this.activity.items.keys.length % 2 === 0) ? this.activity.items.keys.length / 2 : Math.floor(this.activity.items.keys.length / 2) + 1
+            // cria um array de respostas/letras que somem. drop usado por primeira letra nunca pode sumir
+            let keys = sampleSize(drop(this.activity.items.keys), size)
+
+            // percorre todo o tamanho de respostas
+            range(0, keys.length).forEach(i => {
+                for (let item in this.activity.items.keys){
+                    if (keys[i] === this.activity.items.keys[item])
+                        // esconde as letras selecionadas
+                        this.activity.items.values[item].hide = true
+                }
+            })
+
+            let values = filter(this.activity.items.values, v => v.hide)
+
+            values[0].focus = true
+            //seta as respostas
+
+            this.setAnswersArray(keys)
+            this.setActivityAttrs({ total_correct_items: keys.length })
+        },
+        dropLettersImage(){
+            
+        },
+        ...mapActions('Activity', ['setActivityAttrs']),
     }
 }
 </script>
