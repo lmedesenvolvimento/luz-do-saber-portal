8f35a33e2d57b5eb7bab65d0b3f58072e37042d4
João Victor de Alencar <joaovictoralencarpascoal@gmail.com>
2019-06-26 16:07:08 -0300

    logica embaralhar

--- src/renderer/components/ui/activities/read/index.js
+++ src/renderer/components/ui/activities/read/index.js
@@ -1,4 +1,5 @@
 export default{
     'ls-activity-grouping': require('@ui/activities/read/games/Grouping.vue').default,
     'ls-activity-read-dragging-syllables': require('@ui/activities/read/games/DraggingSyllables.vue').default,
+    'ls-activity-read-the-words-secret': require('@ui/activities/read/games/TheWordsSecret.vue').default,
 }
