8856a55c2415a81ceed18f57d4addc03104c7e96
eduardo <eduregis1@gmail.com>
2019-06-26 11:30:32 -0300

    criando atividade
    Former-commit-id: f3e56201ad702798ce4c81f8cce9fef93b95ff02

---
+++ src/renderer/components/ui/activities/read/games/CompleteSequence.vue
@@ -0,0 +1,33 @@
+<template>
+    <div class="container-fluid">
+        <b-row align-v="center" align-h="center">
+            <h2>Onde está a bola</h2>         
+        </b-row>
+        <b-row>
+            <b-col class="activity-keys">
+                aaaaa
+            </b-col>                 
+        </b-row>        
+    </div>
+</template>
+<script>
+import { MapMixins, ListMixin, CreateAnswersMixins } from '@ui/activities/mixins'
+import ui from '@/components/ui'
+
+export default {
+    components: { ...ui },    
+    mixins: [MapMixins, ListMixin, CreateAnswersMixins],
+    data () {
+        return {
+            
+        }
+    },
+    mounted() {
+        this.createAnswersArray()
+    },
+}
+</script>
+
+<style>
+
+</style>
