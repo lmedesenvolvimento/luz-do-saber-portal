ef61b78fe2fd6222ac4564925bb0b3a3ae19e347
eduardo <eduregis1@gmail.com>
2019-06-27 10:41:17 -0300

    inserindo o droppable no meio da sentença

--- src/renderer/components/ui/activities/read/games/CompleteSequence.vue
+++ src/renderer/components/ui/activities/read/games/CompleteSequence.vue
@@ -1,7 +1,30 @@
 <template>
     <div class="container-fluid">
-        <b-row align-v="center" align-h="center">
-            <h2>{{ splitedSentence }}</h2>
+        <b-row align-v="center" align-h="center">            
+            <b-col
+                v-for="(key, position) in activity.items.keys" 
+                :key="key.id"
+                :sm="valueColSize"
+                class="item"
+            >
+                <div style="vertical-align: middle">
+                    <h2>{{ splitedSentence[position] }}</h2> 
+                </div>                
+                <div>
+                    <Item
+                        :item="key"
+                        :type="'key'"
+                        :template="activity.item_template.key"
+                    >                        
+                    </Item>
+                </div> 
+                <h2 
+                    v-if="(activity.total_correct_items != splitedSentence.length) &&
+                        (activity.total_correct_items == position + 1)"
+                >
+                    {{ splitedSentence[position + 1] }}
+                </h2>               
+            </b-col>             
         </b-row>
         <b-row>
             <b-col class="activity-keys">
@@ -10,9 +33,9 @@
                         <div 
                             v-for="item in activity.items.values"
                             :key="item.id"
-                            class="item"
+                            class="item card-sm"
                         >
-                            <Item
+                            <Item                                
                                 :item="item"
                                 :type="'value'"
                                 :template="activity.item_template.value"
@@ -41,21 +64,26 @@ export default {
     },
     mounted() {
         this.createAnswersArray(),
-        this.sentence = this.getKeys[0].text;
+        //this.sentence = this.getKeys[0].text;
+        this.sentence = 'Onde está a? bola'
         this.getValues.forEach(element => {            
             this.hiddenElements.push(element.text)
         });
         for(let i = 0; i < this.sentence.length; i++){
-            console.log(this.hiddenElements,this.sentence)           
+            this.searchString(this.hiddenElements,this.sentence[i])           
         }
+        this.splitedSentence.push(this.sentence)
 
     },
     methods: {
         searchString(arr, str) {
             for(let i = 0; i < arr.length;i++){
-                if (arr[i].match(str)) return true;
+                if(arr[i] == str){
+                    let sentences = this.sentence.split(str)
+                    this.splitedSentence.push(sentences[0])
+                    this.sentence = sentences[1]
+                }
             }
-            return false;
         },
     }
 }
