e9eadbca13adb495f97906a77b9c7d2e82c557c2
João Victor de Alencar <joaovictoralencarpascoal@gmail.com>
2019-07-01 15:23:37 -0300

    atividade finalizada
    Former-commit-id: aa1d357f56be88aa5f122356465d931759466398

--- src/renderer/components/ui/activities/read/games/TheWordsSecret.vue
+++ src/renderer/components/ui/activities/read/games/TheWordsSecret.vue
@@ -1,31 +1,33 @@
 <template>
     <div class="container-fluid">        
         <b-row align-h="center" :class="{'column': !horizontal,'reverse': (reverse && horizontal), 'reverse-column': ( reverse && !horizontal )}">
-            <b-col v-if="hasKeys" class="activity-keys">
+            <b-col v-if="hasKeys" class="px-5 activity-keys">
                 <b-row>
-                    <b-col v-for="(item, position) in newArrayAwnsers" :key="position" cols="6">
-                        <b-row>
+                    <b-col v-for="(item, position) in shuffleValues" :key="position" cols="6" class="px-5">
+                        <b-row class="px-5">
                             <b-col v-for="(item2, position2) in item.syllables" :key="position2" class="item" cols="6">
-                                <Item 
+                                <ls-card-display 
                                     v-if="answers"
                                     :item="item2"
-                                    :type="'key'"
-                                    :template="activity.item_template.key"
-                                />
+                                    :bg-color="item.color"
+                                >
+                                    {{ item2.text }}
+                                </ls-card-display>
                             </b-col>
                         </b-row>
                         <div class="card-input card-input-text ">
                             <label>
                                 <b-card 
                                     no-body
+                                    :class="{ 'invalid': item.invalid, 'valid': item.valid }"
                                 >
                                     <b-card-body>
                                         <input
                                             id="input-name"
-                                            v-model="awnsers[position].text"
                                             type="text"
                                             maxlength="11"
-                                            @blur="checkAwnser(awnsers[position],position)"
+                                            @blur="checkAwnser(...arguments, item, position)"
+                                            @keydown="checkAwnserAfterTime(...arguments, item, position, 3)"
                                         />
                                     </b-card-body>
                                 </b-card>
@@ -38,27 +40,27 @@
     </div>
 </template>
 <script>
+import Vue from 'vue'
 import { MapMixins, ListMixin, CreateAnswersMixins } from '@ui/activities/mixins'
-import { shuffle } from 'lodash'
+import { mapActions } from 'vuex'
+import { clone, shuffle } from 'lodash'
+import FormProps from '@ui/form'
+import { constants } from 'http2';
 
 export default {
+    components: { ...FormProps },
     mixins: [MapMixins, ListMixin, CreateAnswersMixins],
     data() {
         return {
-            awnsers: []
-        }
-    },
-    computed: {
-        newArrayAwnsers() {
-            return shuffle(this.activity.items.values)
+            shuffleValues: [],
+            timeoutId: null,
         }
     },
     created () {
-        this.awnsers = this.newArrayAwnsers;
-        this.awnsers.forEach(e => {
-            e.text = ''
-        });
-        this.newArrayAwnsers.forEach(e => {
+        this.shuffleValues = shuffle(this.getValues)
+        this.shuffleValues.forEach(e => {
+            e.valid = false
+            e.invalid = false
             let pos0 = e.syllables[0]
             let pos1 = e.syllables[e.syllables.length-1]
             e.syllables[0] = pos1
@@ -72,24 +74,65 @@ export default {
                     e.syllables[rnd] = temp;
                 }
             }
+            console.log('item', e)
         });
     },
     mounted() {
         this.createAnswersArray()
+        this.addColorsToType('substantivo_comum')
     },
     methods: {
-        checkAwnser(item, position) {
-            if (item.text.length == this.newArrayAwnsers[position].text.length){ 
-                if (item.text == this.newArrayAwnsers[position].text){
-                    console.log(item)
-                    item.valid = true
-                } else {
-                    item.invalid = true
-                }
+        checkAwnserAfterTime(event, item, position, time){
+            if (this.timeoutId) clearTimeout(this.timeoutId)
+            if (event.keyCode === 13) this.checkAwnser(event, item, position)
+            else{
+                this.timeoutId = setTimeout(()=> {
+                    this.checkAwnser(event, item, position)
+                }, time * 1000)
+            } 
+        },
+        checkAwnser(event, item, position) {
+            const updates = clone(this.shuffleValues)
+            if (this.timeoutId) clearTimeout(this.timeoutId)
+            if (event.target.value === ''){
+                return
             }
-            
+            if (event.target.value.toLowerCase() === item.text.toLowerCase()){
+                this.setAnswer({ 
+                    type: item.type, 
+                    data: item.id,
+                    vm: {}
+                })
+                let nextElementEmpty = event
+                // let nextElementEmpty = this.$el.closest('.input-name').querySelector('input:invalid')
+                console.log(nextElementEmpty)
+                // if (nextElementEmpty) {
+                //     nextElementEmpty.focus()
+                // }
+                updates[position].valid = true
+                event.target.disabled = true
+            } else {
+                this.setAnswer({ 
+                    type: 'value', 
+                    data: -1,
+                    vm: {}
+                })
+                updates[position].invalid = true
+                this.removeInvalid(item, 1, position)
+            }            
+            Vue.set(this, 'shuffleValues', updates)
+        },
+        removeInvalid(item, time, position){
+            setTimeout(()=> {
+                const updates = clone(this.shuffleValues)
+                updates[position].invalid = false
+                Vue.set(this, 'shuffleValues', updates)
+            }, time * 1000)
         }
-    },  
+        ,
+        ...mapActions('Activity', ['setAnswer'])
+    },
+
 }
 </script>
 
