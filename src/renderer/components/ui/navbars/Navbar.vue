f9bdbc106df2b33bf88d9615cbd83077f0f9e0e8
rodrigoronin <liramailbox@gmail.com>
2019-07-04 17:16:04 -0300

    Finishing the activity and adding the new icon for back level. I need to address the issue with the notification colors from WriteTheWord.vue
    Former-commit-id: 1fef6d8f44917c67b3b3a4d5b9bacca90af5de9c

--- src/renderer/components/ui/navbars/Navbar.vue
+++ src/renderer/components/ui/navbars/Navbar.vue
@@ -1,7 +1,7 @@
 <template>
     <nav class="navbar navbar-default">
         <b-btn variant="link" :to="custom ? historyBackUnit : historyBack">
-            <div v-if="this.$route.name !== 'module'" class="icon-lista"></div>
+            <div class="icon-prev-screen"></div>
         </b-btn>
 
         <b-btn variant="link" to="/">
