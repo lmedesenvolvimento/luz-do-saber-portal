541137b1b5d38966f9cc4f1c85afc69c4fb9a674
Nathan Souza <ant.nathan15@gmail.com>
2019-07-05 10:51:22 -0300

    started layout in complete-word

---
+++ src/renderer/components/ui/activities/read/games/CompleteWord.vue
@@ -0,0 +1,141 @@
+<template>
+    <div id="complete-word" class="container-fluid">
+        <b-row align-h="center" class="column">
+            <b-col v-if="hasKeys" class="activity-keys">
+                <b-col v-for="(item, position) in newItens" :key="position" cols="12" md="12" class="item">
+                    <b-row align-v="center">
+                        <b-col cols="12" md="3" sm="3">
+                            <async-image class="image" :src="item.images[0].url"></async-image>
+                        </b-col>
+                        <b-col cols="12" md="9" sm="9" class="syllables-row">
+                            <b-row>
+                                <b-col v-for="(syllables, index) in item.syllables" :key="index" cols="12" lg="3" :md="4" :sm="4" class="key-syllables">
+                                    <ls-card-droppable
+                                        v-if="answers"
+                                        class="item letra"
+                                        :item="syllables.syllable"
+                                        :type="'key'"
+                                        :template="activity.item_template.key"
+                                        :custom-validate="validateBySyllabe"
+                                    >
+                                        <template slot="transfer-data">
+                                            {{ syllables.correct ? syllables.syllable.text : dataTransfer.text }}
+                                        </template>
+                                    </ls-card-droppable>
+                                </b-col>
+                            </b-row>
+                        </b-col>
+                    </b-row>
+                </b-col>
+            </b-col>
+            <b-col v-if="hasKeys" class="activity-keys">
+                <b-col class="activity-values" cols="12" lg="5" md="12">
+                    <b-row align-v="center" align-h="center" class="values-container">
+                        <b-col v-for="(item, position) in getValues" :key="position" align-self="center" cols="12" :sm="3" :md="3" lg="4" class="item">
+                            <ls-card-draggable
+                                v-if="answers"
+                                :item="item"
+                                :type="'value'"
+                                :template="activity.item_template.value"
+                            >
+                                {{ item.text }}
+                            </ls-card-draggable>
+                        </b-col>
+                    </b-row>
+                </b-col>
+            </b-col>
+        </b-row>
+    </div>
+</template>
+<script>
+import { MapMixins, ListMixin, CreateAnswersMixins } from '@ui/activities/mixins'
+import ui from '@/components/ui'
+import AsyncImage from '@ui/AsyncImage'
+import Item from '@/components/ui/items/Item'
+import { mapState, mapActions } from 'vuex'
+
+import FormProps from '@ui/form'
+
+
+export default {
+    components: {
+        ...FormProps,
+        AsyncImage,
+    },
+    mixins: [MapMixins, ListMixin, CreateAnswersMixins],
+    data() {
+        return {
+            newItens: [],
+            dataTransfer: {text: 'AA'},
+        }
+    },
+    mounted() {
+        this.createAnswersArray()
+        this.checkSyllabes(this.getKeys, this.getValues)
+        console.log(this.activity.item_template.value.tags)
+        for (let item1 of this.activity.items.keys){
+            this.newItens.push(JSON.parse(JSON.stringify(item1)));
+        }
+        for (let item1 of this.newItens){
+            let i = 0
+            for (let item2 of item1.syllables){
+                item2.syllable = item1.syllables[i]
+                item2.syllable.word = item1.text
+                item2.correct = false
+                i++
+            }
+        }
+    },
+    methods: {
+        checkSyllabes(keys, values){
+            let keySyllables = keys[0].syllables
+
+            for(let i=0; i<keySyllables.length; i++){
+                for(let j = 0; j<values.length; j++){
+                    if(keySyllables[i].text === values[j].text)
+                        console.log(keySyllables[i].text)
+                }
+            }
+        },
+        // validateBySyllabe(transferData, nativeElement, vm){
+        //     this.dataTransfer = transferData
+        //     if (transferData.text === vm.item.text){
+        //         vm.valid = true;
+        //         transferData.valid = true;
+        //         for (let item1 of this.newItens){
+        //             for(let item2 of item1.syllables){
+        //                 if(item2.syllable.text === transferData.text){
+        //                     item2.correct = true
+        //                 }
+        //             }
+        //         }
+        //         for (let item1 of this.newItens){
+        //             if (item1.text === vm.item.word){
+        //                 for (let item2 of item1.syllables){
+        //                     if (item2.correct === false){
+        //                         return;
+        //                     }
+        //                 }
+        //             }
+        //         }
+        //         vm.setAnswer({
+        //             type: 'value',
+        //             data: transferData.id,
+        //             vm: this
+        //         })
+        //     } else {
+        //         vm.invalid = true;
+        //         transferData.invalid = true
+        //         vm.setAnswer({
+        //             type: 'value',
+        //             data: -1,
+        //             vm: this
+        //         })
+        //     }
+        // },
+    },
+
+}
+</script>
+<style>
+</style>
