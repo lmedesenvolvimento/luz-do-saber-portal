bd1697597262ce69520ceef8ed83517b10563323
eduardo <eduregis1@gmail.com>
2019-07-09 09:10:27 -0300

    colocando um display ao redor dos elementos arrastáveis

--- src/renderer/components/ui/activities/read/games/Accentuation.vue
+++ src/renderer/components/ui/activities/read/games/Accentuation.vue
@@ -1,11 +1,24 @@
 <template>
     <div class="container-fluid">        
-        <b-row align-h="center" :class="{'column': !horizontal,'reverse': (reverse && horizontal), 'reverse-column': ( reverse && !horizontal )}">
-            <b-col v-if="hasKeys" class="activity-keys">
+        <b-row align-h="center">            
+            <b-col class="activity-values">
+                <ls-card-display style="width: 150px">
+                    <b-row>
+                        <b-col v-for="(item, position) in getValues" :key="position" align-self="center" :md="valueColSize" :sm="6" class="item"> 
+                            <Item 
+                                v-if="answers"
+                                :item="item"
+                                :type="'value'"
+                                :template="activity.item_template.value"
+                            />
+                        </b-col>                    
+                    </b-row> 
+                </ls-card-display> 
+            </b-col>
+            <b-col class="activity-keys">
                 <b-row>
-                    <b-col v-for="(item, position) in getKeys" :key="position" :sm="keyColSize" class="item"> 
+                    <b-col v-for="(item, position) in getKeys" :key="position" class="item"> 
                         <Item 
-                            v-if="answers"
                             :item="item"
                             :type="'key'"
                             :template="activity.item_template.key"
@@ -13,30 +26,19 @@
                     </b-col>
                 </b-row>
             </b-col>
-            <b-col 
-                :class="{ 
-                    'flex-4': getValues.length >= 12 
-                }"
-                class="activity-values" 
-            >
-                <b-row>
-                    <b-col v-for="(item, position) in getValues" :key="position" align-self="center" :md="valueColSize" :sm="6" class="item"> 
-                        <Item 
-                            v-if="answers"
-                            :item="item"
-                            :type="'value'"
-                            :template="activity.item_template.value"
-                        />
-                    </b-col>                    
-                </b-row>
-            </b-col>
         </b-row>
     </div>
 </template>
 <script>
 import { MapMixins, ListMixin, CreateAnswersMixins } from '@ui/activities/mixins'
+import ui from '@/components/ui'
+import alerts from '@/components/alerts'
 
 export default {
+    components: {
+        ...ui,
+        ...alerts
+    },
     mixins: [MapMixins, ListMixin, CreateAnswersMixins],
     mounted() {
         this.createAnswersArray()
