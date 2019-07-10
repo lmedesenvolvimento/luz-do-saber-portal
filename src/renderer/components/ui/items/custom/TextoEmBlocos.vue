e40db85be1b150ac989f4e682b9e8f6b347cd558
João Victor de Alencar <joaovictoralencarpascoal@gmail.com>
2019-07-10 10:34:54 -0300

    ajustando espaçamento de novo
    Former-commit-id: d210c9a30d7791a9e1792a9396bbda9bdb5e0c64

--- src/renderer/components/ui/items/custom/TextoEmBlocos.vue
+++ src/renderer/components/ui/items/custom/TextoEmBlocos.vue
@@ -1,6 +1,10 @@
 <template>
     <div class="texto-em-blocos">
-        <ls-card-display>{{ item.text }}</ls-card-display>
+        <ls-card-display>
+            <div class="text">
+                {{ item.text }}
+            </div>
+        </ls-card-display>
     </div>
 </template>
 <script>
@@ -14,8 +18,11 @@ export default {
     .card--display {
         width: 100% !important;
         .card-body {
-            padding-left: 2.4rem !important;
-            letter-spacing: 1.2rem;
+            .text{
+                padding: 0 1rem;
+                letter-spacing: 1.2rem;
+                margin-right: -1.2rem !important;
+            }
         }
     }
 }
