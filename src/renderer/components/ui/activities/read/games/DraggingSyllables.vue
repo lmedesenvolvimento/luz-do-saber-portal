76328b051fcea0e35382c4bf390c73bac1b5ee68
João Victor de Alencar <joaovictoralencarpascoal@gmail.com>
2019-07-18 16:25:58 -0300

    corrigindo validação para 13 respostas em vez de 4
    Former-commit-id: ff5182dd2b20ee15efd11c6c514a7abfe609a75d

--- src/renderer/components/ui/activities/read/games/DraggingSyllables.vue
+++ src/renderer/components/ui/activities/read/games/DraggingSyllables.vue
@@ -105,15 +105,6 @@ export default {
                         }
                     }
                 }
-                for (let item1 of this.newItens){
-                    if (item1.text === vm.item.word){
-                        for (let item2 of item1.syllables){
-                            if (item2.correct === false){
-                                return;
-                            }
-                        }
-                    }
-                }
                 vm.setAnswer({
                     type: 'value',
                     data: transferData.id,
