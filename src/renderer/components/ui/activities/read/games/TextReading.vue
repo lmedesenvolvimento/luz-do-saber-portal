1b5eb0feb1040b164d2c25c7b4571f599b893728
eduardo <eduregis1@gmail.com>
2019-07-25 14:55:49 -0300

    transportando o código da fase para o componente card-audio-listen

--- src/renderer/components/ui/activities/read/games/TextReading.vue
+++ src/renderer/components/ui/activities/read/games/TextReading.vue
@@ -1,51 +1,39 @@
 <template>
     <div class="container-fluid">        
         <b-row align-h="center" align-v="center">
-            <b-col class="item" cols="10"> 
-                <div class="card--text-audio">
-                    <div :class="activity.item_template.key.font_size">
-                        <ls-card-display>
-                            <slot>
-                                <b-col>
-                                    <b-row>
-                                        <b-col cols="7">
-                                            <pre>{{ item.text }}</pre>
-                                        </b-col>
-                                        <b-col>
-                                            <async-image :src="item.images.length ? item.images[0].url : null" />
-                                        </b-col>
-                                    </b-row>                            
-                                    <hr>
-                                    <ls-card-audio :item="item" class="plyr-flat" />
-                                </b-col>                
-                            </slot>                        
-                        </ls-card-display>
-                    </div>                    
-                </div>
+            <b-col cols="9">
+                <b-row>
+                    <b-col v-for="(item, position) in getKeys" :key="position" :sm="keyColSize" class="item"> 
+                        <Item                             
+                            :item="item"
+                            :type="'key'"
+                            :template="activity.item_template.key"
+                        />                        
+                    </b-col>
+                </b-row>
             </b-col>
         </b-row>
     </div>
 </template>
 <script>
+
+import { mapState, mapActions } from 'vuex'
 import ui from '@/components/ui'
 import AsyncImage from '@ui/AsyncImage'
 import { MapMixins, ListMixin, CreateAnswersMixins } from '@ui/activities/mixins'
 
 export default {
     components: { 
-        ...ui,
-        AsyncImage    
+        ...ui
     },
-    mixins: [MapMixins, ListMixin, CreateAnswersMixins],
-    data(){
-        return {
-            item: Object
-        }
-    },
+    mixins: [MapMixins, ListMixin, CreateAnswersMixins],    
     mounted() {
-        this.createAnswersArray()
-        this.item = this.getKeys[0]
+        this.createAnswersArray() 
+        
     },
+    methods: {
+        ...mapActions('Activity', ['triggerSuccess']),
+    }
 }
 </script>
 
