609ef08cf202ff4b2a96d147614c6e93a8a5a9a2
eduardo <eduregis1@gmail.com>
2019-07-25 09:52:29 -0300

    setup de atividade
    Former-commit-id: e0eb3b27f221c68845de409ebe965b9c8c3537b2

---
+++ src/renderer/components/ui/activities/read/games/TextReading.vue
@@ -0,0 +1,19 @@
+<template>
+    <div class="container-fluid">        
+        aaaaa
+    </div>
+</template>
+<script>
+import { MapMixins, ListMixin, CreateAnswersMixins } from '@ui/activities/mixins'
+
+export default {
+    mixins: [MapMixins, ListMixin, CreateAnswersMixins],
+    mounted() {
+        this.createAnswersArray()
+    },
+}
+</script>
+
+<style>
+
+</style>
