16c4b90931cc113992344a44837f9a6b64280c47
eduardo <eduregis1@gmail.com>
2019-07-25 11:32:08 -0300

    ajuste de formatação de texto

--- src/renderer/components/ui/activities/read/games/TextReading.vue
+++ src/renderer/components/ui/activities/read/games/TextReading.vue
@@ -3,22 +3,24 @@
         <b-row align-h="center" align-v="center">
             <b-col v-for="(item, position) in getKeys" :key="position" :sm="keyColSize" class="item"> 
                 <div class="card--text-audio">
-                    <ls-card-display>
-                        <slot>
-                            <b-col>
-                                <b-row>
-                                    <b-col cols="7">
-                                        <p>{{ item.text }}</p>
-                                    </b-col>
-                                    <b-col>
-                                        <async-image :src="item.images.length ? item.images[0].url : null" />
-                                    </b-col>
-                                </b-row>                            
-                                <hr>
-                                <ls-card-audio :item="item" class="plyr-flat" />
-                            </b-col>                
-                        </slot>                        
-                    </ls-card-display>
+                    <div :class="activity.item_template.key.font_size">
+                        <ls-card-display>
+                            <slot>
+                                <b-col>
+                                    <b-row>
+                                        <b-col cols="7">
+                                            <pre>{{ item.text }}</pre>
+                                        </b-col>
+                                        <b-col>
+                                            <async-image :src="item.images.length ? item.images[0].url : null" />
+                                        </b-col>
+                                    </b-row>                            
+                                    <hr>
+                                    <ls-card-audio :item="item" class="plyr-flat" />
+                                </b-col>                
+                            </slot>                        
+                        </ls-card-display>
+                    </div>                    
                 </div>
             </b-col>
         </b-row>
