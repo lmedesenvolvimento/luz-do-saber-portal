da77d67d7a25e32e7dd7361d41963f4be04dfbbf
eduardo <eduregis1@gmail.com>
2019-07-25 11:05:54 -0300

    montando layout com imagem, texto e audio
    Former-commit-id: 9da53b60fc81bc9dee4caf8f2081fa4f667642e1

--- src/renderer/components/ui/activities/read/games/TextReading.vue
+++ src/renderer/components/ui/activities/read/games/TextReading.vue
@@ -1,12 +1,39 @@
 <template>
     <div class="container-fluid">        
-        aaaaa
+        <b-row align-h="center" align-v="center">
+            <b-col v-for="(item, position) in getKeys" :key="position" :sm="keyColSize" class="item"> 
+                <div class="card--text-audio">
+                    <ls-card-display>
+                        <slot>
+                            <b-col>
+                                <b-row>
+                                    <b-col cols="7">
+                                        <p>{{ item.text }}</p>
+                                    </b-col>
+                                    <b-col>
+                                        <async-image :src="item.images.length ? item.images[0].url : null" />
+                                    </b-col>
+                                </b-row>                            
+                                <hr>
+                                <ls-card-audio :item="item" class="plyr-flat" />
+                            </b-col>                
+                        </slot>                        
+                    </ls-card-display>
+                </div>
+            </b-col>
+        </b-row>
     </div>
 </template>
 <script>
+import ui from '@/components/ui'
+import AsyncImage from '@ui/AsyncImage'
 import { MapMixins, ListMixin, CreateAnswersMixins } from '@ui/activities/mixins'
 
 export default {
+    components: { 
+        ...ui,
+        AsyncImage    
+    },
     mixins: [MapMixins, ListMixin, CreateAnswersMixins],
     mounted() {
         this.createAnswersArray()
