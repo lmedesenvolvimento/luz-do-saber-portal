ca41388ab13a24cfd5ab833201696a8ab4d20f76
Damis Garcia <damisgarcia@gmail.com>
2019-07-26 10:10:32 -0300

    init eject code frontpage to new components

--- src/renderer/components/pages/ModuleBibliotecaPage.vue
+++ src/renderer/components/pages/ModuleBibliotecaPage.vue
@@ -1,7 +1,7 @@
 <template>
     <div
-        id="escrever"
-        class="page-container escrever"
+        id="biblioteca"
+        class="page-container biblioteca"
     >
         <navbar
             navbar-title="Escrever"
@@ -9,8 +9,9 @@
             :navbar-icon="getModuleImage"
         />
         <div class="page-container-wrap-spacing">
-            <p>Anim veniam esse excepteur nostrud ad eiusmod. Quis et irure ipsum fugiat quis ullamco enim eiusmod nulla. Ad sunt aliqua pariatur ut commodo laborum quis tempor.</p>
-            <router-link :to="{ name: 'letter' }">Carta</router-link>
+            <b-card title="Lorem ipsums">
+                <p>Anim veniam esse excepteur nostrud ad eiusmod. Quis et irure ipsum fugiat quis ullamco enim eiusmod nulla. Ad sunt aliqua pariatur ut commodo laborum quis tempor.</p>
+            </b-card>
         </div>
     </div>
 </template>
@@ -21,7 +22,7 @@ export default {
     components: { Navbar },
     computed: {
         getModuleImage(){
-            return require('@/assets/images/btn-write.png')
+            return require('@/assets/images/btn-books.png')
         }
     }
 }
