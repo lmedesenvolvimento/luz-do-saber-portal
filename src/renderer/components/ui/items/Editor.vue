2ba4c70862bd7c494e2215c09270460be89a9ded
João Victor de Alencar <joaovictoralencarpascoal@gmail.com>
2019-07-30 09:19:07 -0300

    editor sem estilização
    Former-commit-id: 3476c63536955b1dd12aa9f8aa8a2dace0218762

--- src/renderer/components/ui/items/Editor.vue
+++ src/renderer/components/ui/items/Editor.vue
@@ -1,92 +1,46 @@
 <template>
     <div>
-        <!-- <div id="editor" ref="editor"></div> -->
-        <div id="toolbar-container">
+        <div id="toolbar">
             <select class="ql-font">
-                <option selected="Roboto">Roboto</option>
-                <option value="Montserrat">Montserrat</option>
+                <option selected>Aref Ruqaa</option>
+                <option value="mirza">Mirza</option>
+                <option value="roboto">Roboto</option>
             </select>
+            <select class="ql-size">
+                <option value="small">Small</option>
+                <option selected value="medium">Medium</option>
+                <option value="large">large</option>
+                <option value="huge">Huge</option>
+            </select>
+            <button class="ql-bold">N</button>
+            <button class="ql-italic">I</button>
+            <button class="ql-underline">S</button>
+            <button class="ql-align" value="left">left</button>
+            <button class="ql-align" value="center">center</button>
+            <button class="ql-align" value="right">right</button>
+            <span class="ql-formats">
+                <button class="ql-list" value="ordered">Lista Ordenada</button>
+                <button class="ql-list" value="bullet">Lista Não-ordenzda</button>
+            </span>
         </div>
-        <div id="editor-container">
-            <p>When Mr. Bilbonificence, there was much talk and excitement in Hobbiton.</p>
-        </div>
+        <div id="editor"></div>
     </div>
 </template>
 <script>
 import Quill from 'quill';
 import 'quill/dist/quill.snow.css'
 export default {
-    props: {
-        value: {
-            type: String,
-            default: ''
-        }
-    }, 
-
-    data() {
-        return {
-            editor: null
-        };
-    },
     mounted() {
-        var Font = Quill.import('formats/font');
-        // We do not add Aref Ruqaa since it is the default
-        Font.whitelist = ['Roboto', 'Montserrat'];
-        Quill.register(Font, true);
-
-        var quill = new Quill('#editor-container', {
+        var quill = new Quill('#editor', {
             modules: {
-                toolbar: '#toolbar-container'
+                toolbar: '#toolbar'
             },
-            theme: 'snow'
         });
-
-        // var Font = Quill.import('formats/font');
-        // Font.whitelist = ['Roboto'];
-        // Quill.register(Font, true);
-
-        // this.editor = new Quill(this.$refs.editor, {
-        //     modules: {
-        //         toolbar: [
-        //             [{ header: [1, 2, 3, 4, false] }],
-        //             ['bold', 'italic', 'underline'],
-        //             [{fonts:['Roboto', 'aseasease', 'aseaseaseas']}]
-        //         ],
-        //     },
-        //     theme: 'snow',
-        //     formats: ['bold', 'underline', 'header', 'italic'],
-        // });
-
-        // this.editor.root.innerHTML = this.value;
-
-        // this.editor.on('text-change', () => this.update());
-    },
-
-    methods: {
-        update() {
-            this.$emit('input', this.editor.getText() ? this.editor.root.innerHTML : '');
-        }
     }
 }
 </script>
 <style lang="scss">
-    #editor-container {
-        font-family: "Roboto";
-        font-size: 18px;
-        height: 375px;
-    }
-    @import url('https://fonts.googleapis.com/css?family=Roboto&display=swap');
-    @import url('https://fonts.googleapis.com/css?family=Montserrat&display=swap');
-    #toolbar-container .ql-font span[data-label="Roboto"]::before {
-        font-family: "Roboto";
-    }
-    .ql-font-roboto {
-        font-family: "Roboto";
-    }
-    #toolbar-container .ql-font span[data-label="Montserrat"]::before {
-        font-family: "Montserrat";
-    }
-    .ql-font-Montserrat {
-        font-family: "Montserrat";
-    } 
+#editor{
+    background-color: white;
+}
 </style>
