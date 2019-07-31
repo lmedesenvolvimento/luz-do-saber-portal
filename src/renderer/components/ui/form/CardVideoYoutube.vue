ee830bd0ce699e598f10959fec40841587964dcf
lord <m_s_r@outlook.com>
2019-07-31 11:04:59 -0300

    video de abertura generico
    Former-commit-id: 6fcedefad58e9c5884308ee4aa359a3072da4bf1

--- src/renderer/components/ui/form/CardVideoYoutube.vue
+++ src/renderer/components/ui/form/CardVideoYoutube.vue
@@ -6,7 +6,7 @@
             <vue-plyr>
                 <div class="plyr__video-embed">
                     <iframe
-                        src="https://www.youtube.com/embed/bTqVqk7FSmY?iv_load_policy=3&modestbranding=1&playsinline=1&showinfo=0&rel=0&enablejsapi=1"
+                        src="https://www.youtube.com/watch?v=GO94XCuXEqs?iv_load_policy=3&playsinline=1&showinfo=0&rel=0&enablejsapi=1"
                         allowfullscreen allowtransparency allow="autoplay"
                     >
                     </iframe>
