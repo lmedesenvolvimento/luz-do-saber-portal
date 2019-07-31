7fe776e66ebbde797e8d0f171508cf518fb97268
lord <m_s_r@outlook.com>
2019-07-31 11:03:22 -0300

    player yt adicionado, esperando tipo correto
    Former-commit-id: 0e6ab3d290b28c85dc132c164eb9b07915029eb8

---
+++ src/renderer/components/ui/form/CardVideoYoutube.vue
@@ -0,0 +1,26 @@
+<template>
+    <div v-if="item" class="card--youtube" :class="$attrs.class">
+        <b-card
+            no-body
+        >
+            <vue-plyr>
+                <div class="plyr__video-embed">
+                    <iframe
+                        src="https://www.youtube.com/embed/bTqVqk7FSmY?iv_load_policy=3&modestbranding=1&playsinline=1&showinfo=0&rel=0&enablejsapi=1"
+                        allowfullscreen allowtransparency allow="autoplay"
+                    >
+                    </iframe>
+                </div>
+            </vue-plyr>
+        </b-card>
+    </div>
+</template>
+
+<script>
+import RadioInput from './RadioInput.vue'
+
+export default {
+    mixins: [RadioInput],
+}
+</script>
+
