22027f2296ab7421f9d6fb902bd131b6e5455545
lord <m_s_r@outlook.com>
2019-07-31 11:03:22 -0300

    player yt adicionado, esperando tipo correto

---
+++ src/renderer/components/ui/items/Video.vue
@@ -0,0 +1,22 @@
+<template>
+    <div>
+        <ls-card-video-youtube
+            v-if="type === 'key'"
+            class="col-sm"
+            label="item.text"
+            :item="item"
+            :name="activity.type.slug"
+            :template="template"
+        >
+        </ls-card-video-youtube>
+    </div>
+</template>
+<script>
+import { ItemProps } from './index.js'
+
+export default {
+    mixins: [ItemProps],
+    mounted() {
+    },
+}
+</script>
