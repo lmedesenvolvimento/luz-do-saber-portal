3346ce766e6412d104214477e44170e27e20ee26
João Victor de Alencar <joaovictoralencarpascoal@gmail.com>
2019-08-01 11:59:39 -0300

    estilização inicial
    Former-commit-id: 7b6961cd6b365fa29ad46a880c2a3acbda7da9b9

--- src/renderer/components/ui/items/Editor.vue
+++ src/renderer/components/ui/items/Editor.vue
@@ -1,26 +1,72 @@
 <template>
-    <div>
+    <div id="custom-editor">
         <div id="toolbar">
-            <select class="ql-font">
-                <option selected>Montserrat</option>
-                <option value="Roboto">Roboto</option>
-            </select>
-            <select class="ql-size">
-                <option value="small">Pequeno</option>
-                <option selected value="medium">Médio</option>
-                <option value="large">Grande</option>
-                <option value="huge">Enorme</option>
-            </select>
-            <button class="ql-bold">N</button>
-            <button class="ql-italic">I</button>
-            <button class="ql-underline">S</button>
-            <button class="ql-align" selected>left</button>
-            <button class="ql-align" value="center">center</button>
-            <button class="ql-align" value="right">right</button>
-            <span class="ql-formats">
-                <button class="ql-list" value="ordered">Lista Ordenada</button>
-                <button class="ql-list" value="bullet">Lista Não-ordenada</button>
-            </span>
+            <b-row class="fill">
+                <b-col class="col-font-style">
+                    <ls-card-display>
+                        <select class="ql-font btn-editor">
+                            <option selected class="ql-font-Montserrat">Montserrat</option>
+                            <option value="Roboto" class="ql-font-Roboto">Roboto</option>
+                        </select>
+                    </ls-card-display>
+                </b-col>
+                <b-col class="col-font-size">
+                    <ls-card-display>
+                        <select class="ql-size btn-editor">
+                            <option value="small">8</option>
+                            <option selected value="medium">12</option>
+                            <option value="large">18</option>
+                            <option value="huge">24</option>
+                        </select>
+                    </ls-card-display>
+                </b-col>
+                <b-col class="col-text-style">
+                    <ls-card-display>
+                        <b-col>
+                            <b-row>
+                                <b-col class="no-padding">
+                                    <button class="ql-bold btn-editor b-r"><b>N</b></button>
+                                </b-col>
+                                <b-col class="no-padding">
+                                    <button class="ql-italic btn-editor b-r"><i>I</i></button>
+                                </b-col>
+                                <b-col class="no-border">                   
+                                    <button class="ql-underline btn-editor"><u>S</u></button>
+                                </b-col>
+                            </b-row>
+                        </b-col>
+                    </ls-card-display>
+                </b-col>
+                <b-col class="col-text-align">
+                    <ls-card-display>
+                        <b-row>
+                            <b-col class="no-padding"> 
+                                <button class="ql-align btn-editor b-r " selected>left</button>
+                            </b-col>
+                            <b-col class="no-padding">
+                                <button class="ql-align btn-editor b-r" value="center">center</button>
+                            </b-col>
+                            <b-col class="no-border">                    
+                                <button class="ql-align btn-editor" value="right">right</button>
+                            </b-col> 
+                        </b-row>
+                    </ls-card-display>
+                </b-col>
+                <b-col class="col-list">
+                    <ls-card-display>
+                        <span class="ql-formats">
+                            <b-row>
+                                <b-col class="no-padding">
+                                    <button class="ql-list btn-editor b-r" value="ordered">L1</button>
+                                </b-col>
+                                <b-col class="no-border">
+                                    <button class="ql-list btn-editor" value="bullet">l2</button>
+                                </b-col>
+                            </b-row>
+                        </span>
+                    </ls-card-display>
+                </b-col>
+            </b-row>
         </div>
         <div id="editor"></div>
     </div>
@@ -48,19 +94,74 @@ export default {
 </script>
 <style lang="scss">
 @import url('https://fonts.googleapis.com/css?family=Montserrat|Roboto&display=swap');
-#editor{
-    background-color: white;
-    font-family: "Montserrat";
-    // font-size: 18px;
-    height: 300px;
-    .ql-editor{
-        height: 100%;
+#custom-editor{
+    #toolbar{
+        position: relative;
+        display: flex;
+        flex-direction: column;
+        align-items: center;
+        min-height: 100px;
+        background-color: #fafafa;
+        border-bottom: 1.5px solid #ddd;
+        padding: 0px 8px;
+        flex: 1;
     }
-}
-.ql-font-Montserrat {
-    font-family: 'Montserrat', sans-serif;
-}
-.ql-font-Roboto {
-    font-family: 'Roboto', sans-serif;
+    #editor{
+        background-color: white;
+        font-family: "Montserrat";
+        max-width: 100%;
+        height: 300px;
+        .ql-editor{
+            height: 100%;
+        }
+    }
+    .ql-font-Montserrat {
+        font-family: 'Montserrat', sans-serif;
+    }
+    .ql-font-Roboto {
+        font-family: 'Roboto', sans-serif;
+    }
+    .btn-editor {
+        background: Transparent no-repeat;
+        border: none;
+        cursor:pointer;
+        overflow: hidden;
+        outline:none;
+        font-size: 1.3rem;
+        width: 100%;
+    }
+    .card--display{
+        max-height: 90px;
+    }
+    .card-body{
+        padding: 0.9rem 1rem !important;
+    }
+    .col-font-style{
+        max-width: 22%;
+    }
+    .col-font-size{
+        max-width: 11%;
+    }
+    .col-text-style{
+        max-width: 15%;
+    }
+    .col-text-align{
+        max-width: 24%;
+    }
+    .col-list{
+        max-width: 15%;
+    }
+    .no-padding{
+        padding: 0;
+        border-right: solid black 1px;
+    }
+    .no-border{
+        border: none;
+        padding: 0;
+    }
+    .row{
+        justify-content: center;
+        align-items: center;
+    }
 }
 </style>
