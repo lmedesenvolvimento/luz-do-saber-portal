636d4a22af42b6558d793620a5486bd9159886f6
João Victor de Alencar <joaovictoralencarpascoal@gmail.com>
2019-08-05 11:17:36 -0300

    estilização com icones
    Former-commit-id: abe2dcb9646fc55c464322beb6d6029df45bb8c7

--- src/renderer/components/ui/items/Editor.vue
+++ src/renderer/components/ui/items/Editor.vue
@@ -1,72 +1,72 @@
 <template>
     <div id="custom-editor">
         <div id="toolbar">
-            <b-row class="fill">
-                <b-col class="col-font-style">
+            <div class="linha">
+                <div class="button-contaniner">
                     <ls-card-display>
                         <select class="ql-font btn-editor">
                             <option selected class="ql-font-Montserrat">Montserrat</option>
                             <option value="Roboto" class="ql-font-Roboto">Roboto</option>
                         </select>
                     </ls-card-display>
-                </b-col>
-                <b-col class="col-font-size">
+                </div>
+                <div class="button-contaniner">
                     <ls-card-display>
                         <select class="ql-size btn-editor">
-                            <option value="small">8</option>
-                            <option selected value="medium">12</option>
-                            <option value="large">18</option>
-                            <option value="huge">24</option>
+                            <option value="small">12</option>
+                            <option selected value="medium">18</option>
+                            <option value="large">24</option>
+                            <option value="huge">40</option>
                         </select>
                     </ls-card-display>
-                </b-col>
-                <b-col class="col-text-style">
+                </div>
+                <div class="button-contaniner">
                     <ls-card-display>
-                        <b-col>
-                            <b-row>
-                                <b-col class="no-padding">
-                                    <button class="ql-bold btn-editor b-r"><b>N</b></button>
-                                </b-col>
-                                <b-col class="no-padding">
-                                    <button class="ql-italic btn-editor b-r"><i>I</i></button>
-                                </b-col>
-                                <b-col class="no-border">                   
-                                    <button class="ql-underline btn-editor"><u>S</u></button>
-                                </b-col>
-                            </b-row>
-                        </b-col>
+                        <div>
+                            <div class="linha">
+                                <button class="ql-bold btn-editor coluna b-right">
+                                    <img :src="bold" alt="Negrito">
+                                </button>
+                                <button class="ql-italic btn-editor coluna b-right">
+                                    <img :src="italic" alt="Itálico">
+                                </button>
+                                <button class="ql-underline btn-editor coluna">
+                                    <img :src="underline" alt="Sublinhado">
+                                </button>
+                            </div>
+                        </div>
                     </ls-card-display>
-                </b-col>
-                <b-col class="col-text-align">
+                </div>
+                <div class="button-contaniner">
                     <ls-card-display>
-                        <b-row>
-                            <b-col class="no-padding"> 
-                                <button class="ql-align btn-editor b-r " selected>left</button>
-                            </b-col>
-                            <b-col class="no-padding">
-                                <button class="ql-align btn-editor b-r" value="center">center</button>
-                            </b-col>
-                            <b-col class="no-border">                    
-                                <button class="ql-align btn-editor" value="right">right</button>
-                            </b-col> 
-                        </b-row>
+                        <div class="linha">
+                            <button class="ql-align btn-editor coluna b-right" selected>
+                                <img :src="left" alt="Alinhamento à esquerda">
+                            </button>
+                            <button class="ql-align btn-editor coluna b-right" value="center">
+                                <img :src="center" alt="Alinhamento ao centro">
+                            </button>
+                            <button class="ql-align btn-editor coluna" value="right">
+                                <img :src="right" alt="Alinhamento à direita">
+                            </button>
+                        </div>
                     </ls-card-display>
-                </b-col>
-                <b-col class="col-list">
+                </div>
+                <div class="button-contaniner">
                     <ls-card-display>
                         <span class="ql-formats">
-                            <b-row>
-                                <b-col class="no-padding">
-                                    <button class="ql-list btn-editor b-r" value="ordered">L1</button>
-                                </b-col>
-                                <b-col class="no-border">
-                                    <button class="ql-list btn-editor" value="bullet">l2</button>
-                                </b-col>
-                            </b-row>
+                            <div class="linha">
+                                <button class="ql-list btn-editor coluna b-right" value="bullet">
+                                    <img :src="bullet" alt="Lista não ordenada">
+                                </button>
+                                <button class="ql-list btn-editor coluna" value="ordered">
+                                    <img :src="ordered" alt="Lista ordenada">
+                                </button>
+                            </div>
                         </span>
                     </ls-card-display>
-                </b-col>
-            </b-row>
+                </div>
+            </div>
         </div>
         <div id="editor"></div>
     </div>
@@ -83,13 +83,40 @@ Quill.register(Font, true);
 
 export default {
     components: { ...FormProps },
+    computed: {
+        bold() {
+            return require('@/assets/images/icons/escrever/editor/N.png')
+        },
+        italic() {
+            return require('@/assets/images/icons/escrever/editor/I.png')
+        },
+        underline() {
+            return require('@/assets/images/icons/escrever/editor/S.png')
+        },
+        left() {
+            return require('@/assets/images/icons/escrever/editor/esquerda.png')
+        },
+        center() {
+            return require('@/assets/images/icons/escrever/editor/centro.png')
+        },
+        right() {
+            return require('@/assets/images/icons/escrever/editor/direita.png')
+        },
+        bullet() {
+            return require('@/assets/images/icons/escrever/editor/marcador 1.png')
+        },
+        ordered() {
+            return require('@/assets/images/icons/escrever/editor/marcador 2.png')
+        }
+    },
     mounted() {
         var quill = new Quill('#editor', {
             modules: {
                 toolbar: '#toolbar'
             },
+            placeholder: 'Insira seu texto aqui!'
         });
-    }
+    },
 }
 </script>
 <style lang="scss">
@@ -109,6 +136,7 @@ export default {
     #editor{
         background-color: white;
         font-family: "Montserrat";
+        font-size: 18px;
         max-width: 100%;
         height: 300px;
         .ql-editor{
@@ -124,7 +152,7 @@ export default {
     .btn-editor {
         background: Transparent no-repeat;
         border: none;
-        cursor:pointer;
+        cursor: pointer;
         overflow: hidden;
         outline:none;
         font-size: 1.3rem;
@@ -134,32 +162,26 @@ export default {
         max-height: 90px;
     }
     .card-body{
-        padding: 0.9rem 1rem !important;
+        padding: 0.3rem 0.6rem;
     }
-    .col-font-style{
-        max-width: 22%;
+    .ql-font, .ql-size{
+        height: 46px;
     }
-    .col-font-size{
-        max-width: 11%;
-    }
-    .col-text-style{
-        max-width: 15%;
-    }
-    .col-text-align{
-        max-width: 24%;
-    }
-    .col-list{
-        max-width: 15%;
-    }
-    .no-padding{
-        padding: 0;
+    .b-right{
         border-right: solid black 1px;
+        height: 100%;
     }
-    .no-border{
-        border: none;
-        padding: 0;
+    .coluna{
+        flex-grow: 1;
+        max-width: 100%;
+        height: 46px;
     }
-    .row{
+    .button-contaniner{
+        margin-right: 30px;
+    }
+    .linha{
+        display: flex;
+        flex-grow: 1;
         justify-content: center;
         align-items: center;
     }
