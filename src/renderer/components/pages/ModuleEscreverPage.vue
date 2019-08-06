597a884f8ab9ca0644929724d02b03d83242cc90
eduardo <eduregis1@gmail.com>
2019-08-06 10:06:58 -0300

    substituindo placeholders por icones

--- src/renderer/components/pages/ModuleEscreverPage.vue
+++ src/renderer/components/pages/ModuleEscreverPage.vue
@@ -14,7 +14,7 @@
                 <b-col v-for="theme in getWriteThemes" :key="theme.id" cols="12" md="6">
                     <div class="write-theme-card">
                         <div class="write-theme-card-icon">
-                            <img :src="getModuleImage" alt="">
+                            <img :src="theme.icon" :alt="theme.title">
                         </div>
                         <div class="write-theme-card-title">
                             <div>
@@ -43,16 +43,20 @@ export default {
         getWriteThemes(){
             return [
                 {
-                    title: 'Editor de Texto'
+                    title: 'Editor de Texto',
+                    icon: require('@/assets/images/write-module-icon-editor-texto.png')
                 },
                 {
-                    title: 'Jornal'
+                    title: 'Jornal',
+                    icon: require('@/assets/images/write-module-icon-jornal.png')
                 },
                 {
-                    title: 'Lista'
+                    title: 'Lista',
+                    icon: require('@/assets/images/write-module-icon-lista.png')
                 },
                 {
-                    title: 'Cartão de Aniversário'
+                    title: 'Cartão de Aniversário',
+                    icon: require('@/assets/images/write-module-icon-cartao-aniversario.png')
                 }
             ]
         }       
@@ -65,14 +69,19 @@ export default {
         background-image: url('~@/assets/images/write-module-background.png');
         background-position:  center;
         background-repeat:  no-repeat; 
+        border-radius: 10px;
+        box-shadow: 0px 0px 36px 1px rgba(123, 123, 132, 0.48);
+        color: #777777;
         cursor: pointer;       
         text-align: center;
         width: 378px;
-        height: 188px;
-        border-radius: 10px;
-        margin: 0 auto 100px;
-        box-shadow: 0px 0px 36px 1px rgba(123, 123, 132, 0.48);
+        height: 188px;        
+        margin: 0 auto 100px;        
         padding: 50px;
+        transition: transform 0.2s ease-in-out;
+        &:hover{
+            transform: scale(1.1);
+        }
         .write-theme-card-icon{
             margin: -125px auto 0;
             width: 150px;
@@ -82,17 +91,18 @@ export default {
             border-color: white;
             border-radius: 75px;
             img{
-                width: 150px;
-                height: 150px;
+                width: 140px;
+                height: 140px;
             }
 
         }
         .write-theme-card-title{
-            margin-top: 20px;                                  
-        }
-        transition: transform 0.2s ease-in-out;
-        &:hover{
-            transform: scale(1.1);
+            margin-top: 20px; 
+            height: 80px;
+            display: flex;
+            justify-content: center;
+            align-items: center;         
+                                 
         }
     }
 </style>
