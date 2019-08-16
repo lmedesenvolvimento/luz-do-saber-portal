86c94e2d64700edf16669e0734931c3ac8ee89b6
eduardo <eduregis1@gmail.com>
2019-08-16 10:56:38 -0300

    iniciando pagina dupla
    Former-commit-id: 896312f0dc6d07f29afc0d82eacbbdacacc52b7d

--- src/renderer/components/ui/items/Journal.vue
+++ src/renderer/components/ui/items/Journal.vue
@@ -1,50 +1,133 @@
 <template>
-    <div class="journal-container">
-        <div class="journal">
-            <div class="journal-page-container">
-                <div v-if="singlePage" class="journal-page">
-                    <div class="journal-decoration-1" />
-                    <div class="journal-decoration-2" />
-                    <div class="journal-content">
-                        <div class="journal-header-title">
-                            <h3>INSIRA O NOME DO JORNAL</h3>
-                        </div>
-                        <div class="journal-header-image">
-                        </div>
-                        <div>
-                            <div class="journal-header-story-left">
-                                <div class="journal-header-story-title">
-                                    <h5>TÍTULO DO LADO ESQUERDO</h5>
-                                </div>
-                                <div class="journal-header-story-subtitle">
-                                    <p>Insira o conteúdo da matéria do lado esquerdo</p>
-                                </div>
-                            </div>                                
-                            <div class="journal-header-story-right">
-                                <div class="journal-header-story-title">
-                                    <h5>TÍTULO DO LADO ESQUERDO</h5>
-                                </div>
-                                <div class="journal-header-story-subtitle">
-                                    <p>Insira o conteúdo da matéria do lado esquerdo</p>
+    <b-row class="journal-container">
+        <b-col>
+            <div class="journal">
+                <div v-if="pageName=='capa'" class="journal-page-container journal-single-page">
+                    <div class="journal-page">
+                        <div class="journal-decoration-1" />
+                        <div class="journal-decoration-2" />
+                        <div class="journal-content">
+                            <div class="journal-header-title">
+                                <h3>INSIRA O NOME DO JORNAL</h3>
+                            </div>
+                            <div class="journal-header-image">
+                            </div>
+                            <div>
+                                <div class="journal-header-story-left">
+                                    <div class="journal-header-story-title">
+                                        <h5>TÍTULO DO LADO ESQUERDO</h5>
+                                    </div>
+                                    <div class="journal-header-story-subtitle">
+                                        <p>Insira o conteúdo da matéria do lado esquerdo</p>
+                                    </div>
+                                </div>                                
+                                <div class="journal-header-story-right">
+                                    <div class="journal-header-story-title">
+                                        <h5>TÍTULO DO LADO ESQUERDO</h5>
+                                    </div>
+                                    <div class="journal-header-story-subtitle">
+                                        <p>Insira o conteúdo da matéria do lado esquerdo</p>
+                                    </div>
                                 </div>
                             </div>
                         </div>
+                        <div class="journal-decoration-1" />
                     </div>
-                    <div class="journal-decoration-1" />
+                </div>
+                <div v-if="pageName=='1-2'" class="journal-page-container">
+                    <div class="journal-page">
+                        <div class="journal-decoration-1" />
+                        <div class="journal-decoration-2" />
+                        <div class="journal-content">
+                            <div class="journal-page-1-header">
+                                <h3>Matéria de destaque</h3>
+                                <h5>Subtítulo da matéria</h5>
+                            </div>
+                            <div class="journal-header-image">
+                            </div>
+                            <div>
+                                <div class="journal-header-story-left">
+                                    <div class="journal-header-story-title">
+                                        <h5>TÍTULO DO LADO ESQUERDO</h5>
+                                    </div>
+                                    <div class="journal-header-story-subtitle">
+                                        <p>Insira o conteúdo da matéria do lado esquerdo</p>
+                                    </div>
+                                </div>                                
+                                <div class="journal-header-story-right">
+                                    <div class="journal-header-story-title">
+                                        <h5>TÍTULO DO LADO ESQUERDO</h5>
+                                    </div>
+                                    <div class="journal-header-story-subtitle">
+                                        <p>Insira o conteúdo da matéria do lado esquerdo</p>
+                                    </div>
+                                </div>
+                            </div>
+                        </div>
+                        <div class="journal-decoration-1" />
+                    </div>
                 </div>
             </div>
-        </div>
-        <div class="journal-footer">
-            a
-        </div>
-    </div>
+            <b-row align-h="center" class="journal-footer">
+                <b-col cols="4">
+                    <b-row align-h="center">
+                        <b-button v-b-modal.modal-center class="btn-journal" @click="toPrevPage">
+                            <img :src="prevPage" alt="Página anterior">
+                        </b-button>
+                        <div class="journal-footer-page-name">
+                            <div>
+                                <h3>{{ pageName }}</h3>
+                            </div>
+                        </div>               
+                        <b-button v-b-modal.modal-center class="btn-journal" @click="toNextPage">
+                            <img :src="nextPage" alt="Próxima página">
+                        </b-button>
+                    </b-row>
+                </b-col>  
+            </b-row>
+        </b-col>        
+    </b-row>
 </template>
 
 <script>
 export default {
     data(){
         return {
-            singlePage: true
+            pageName: 'capa'
+        }
+    },
+    computed: {
+        prevPage() {
+            return require('@/assets/images/icons/escrever/icon-prev.png')
+        },
+        nextPage() {
+            return require('@/assets/images/icons/escrever/icon-next.png')
+        }
+    },
+    methods: {
+        toPrevPage() {
+            switch(this.pageName){
+            case '1-2':
+                this.pageName = 'capa'
+                break;
+            case 'verso':
+                this.pageName = '1-2'
+                break;
+            default:
+                break;
+            }
+        },
+        toNextPage() {
+            switch(this.pageName){
+            case 'capa':
+                this.pageName = '1-2'
+                break;
+            case '1-2':
+                this.pageName = 'verso'
+                break;
+            default:
+                break;
+            }
         }
     }
 }
@@ -71,6 +154,8 @@ export default {
         border-radius:10px;
         padding: 5px;
         margin: 0px auto 0 auto;
+    }
+    .journal-single-page{
         width: 50%;        
     }
     .journal-decoration-1{
@@ -124,4 +209,32 @@ export default {
         background-color: #fafafa;
         border-top: 1px solid #ddd;
    }
+    .journal-footer-page-name{
+        border: solid;
+        border-radius: 15px;
+        height: 45px;
+        width: 120px;
+        color: #13c5c4;
+        background-color:white;
+        margin-top: 15px;
+        div{
+            border-radius: 8px;
+            margin: 3px;
+            background-color:#eeeeee;
+            text-align: center;
+        }
+    }
+    .btn-journal{
+        background: Transparent no-repeat;
+        border: none;
+        cursor: pointer;
+        overflow: hidden;
+        outline:none;
+        font-size: 1.3rem;
+        color: #303030;
+        box-shadow: none !important;
+        img{
+            width: 62px;
+        }
+    }
 </style>
