e68c58e6918b2839143beff518e673fafe47ee39
eduardo <eduregis1@gmail.com>
2019-08-19 10:45:56 -0300

    layout estático do verso
    Former-commit-id: cc5bcf10a1912890c4a5ad0902cae90261cf212d

--- src/renderer/components/ui/items/Journal.vue
+++ src/renderer/components/ui/items/Journal.vue
@@ -119,33 +119,35 @@
                         </b-row>                    
                     </div>
                     <div v-else-if="pageName=='verso'" class="journal-page-container journal-single-page">
-                        <div class="journal-page">
+                        <div class="journal-page journal-back-cover">
                             <div class="journal-decoration-1" />
                             <div class="journal-decoration-2" />
-                            <div class="journal-content">
-                                <div class="journal-header-title">
-                                    <h3>INSIRA O NOME DO JORNAL</h3>
-                                </div>
-                                <div class="journal-header-image">
-                                </div>
+                            <div class="journal-content">                                
                                 <div>
-                                    <div class="journal-header-story-left">
-                                        <div class="journal-header-story-title">
-                                            <h5>Título do lado esquerdo</h5>
-                                        </div>
-                                        <div class="journal-header-story-subtitle">
-                                            <p>Insira o conteúdo da matéria do lado esquerdo</p>
-                                        </div>
-                                    </div>                                
-                                    <div class="journal-header-story-right">
-                                        <div class="journal-header-story-title">
-                                            <h5>Título do lado direito</h5>
-                                        </div>
-                                        <div class="journal-header-story-subtitle">
-                                            <p>Insira o conteúdo da matéria do lado direito</p>
-                                        </div>
-                                    </div>
+                                    <b-row>
+                                        <b-col class="journal-back-cover-story-left">
+                                            <div class="journal-story-title">
+                                                <h5>Título do lado esquerdo</h5>
+                                            </div>
+                                            <div class="journal-story-subtitle">
+                                                <p>Insira o conteúdo da matéria do lado esquerdo</p>
+                                            </div>
+                                        </b-col>
+                                        <b-col class="journal-back-cover-story-right">
+                                            <div class="journal-story-title">
+                                                <h5>Título do lado direito</h5>
+                                            </div>
+                                            <div class="journal-story-subtitle">
+                                                <p>Insira o conteúdo da matéria do lado direito</p>
+                                            </div>
+                                        </b-col>
+                                    </b-row>
                                 </div>
+                                <div class="journal-back-cover-image">
+                                </div>                                
+                                <div class="journal-story-cont">
+                                    <p>Insira um conteúdo aqui</p>
+                                </div> 
                             </div>
                             <div class="journal-decoration-1" />
                         </div>
