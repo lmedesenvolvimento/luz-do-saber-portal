c734e92a1e076b68293aee03f3cfa7265f685133
Damis Garcia <damisgarcia@gmail.com>
2019-08-28 13:29:06 -0300

    serving static api
    Former-commit-id: b0abd58db2ad7ae774f7747b32e89d00beca597a

--- src/main/server/index.js
+++ src/main/server/index.js
@@ -1,15 +1,11 @@
 const express = require('express');
-const nocache = require('nocache');
-const Router = require('./router');
 const PORT = 9000;
 
 const boostrapAPI = () => {
     const app = express();
-
-    app.use(nocache())
     
-    Router(app);
-    
+    app.use(express.static('public'));
+        
     app.listen(PORT, () => {
         console.log(`API up listening on port ${PORT}!`)
     });
