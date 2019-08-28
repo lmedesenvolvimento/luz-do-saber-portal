23892f53c6c4e1819e4714b2277b9ea753623ef1
Damis Garcia <damisgarcia@gmail.com>
2019-08-28 14:50:46 -0300

    update server static folder

--- src/main/server/index.js
+++ src/main/server/index.js
@@ -4,7 +4,11 @@ const PORT = 9000;
 const boostrapAPI = () => {
     const app = express();
     
-    app.use(express.static('public'));
+    app.use(express.static('static'));
+
+    app.get('/ping', (req, res) => {
+        res.send('Hello Electron')
+    })
         
     app.listen(PORT, () => {
         console.log(`API up listening on port ${PORT}!`)
