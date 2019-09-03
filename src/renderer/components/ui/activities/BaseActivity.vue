c6e02a58542f0b397be9b0955368453d9eda2492
João Victor de Alencar <joaovictoralencarpascoal@gmail.com>
2019-09-03 12:34:20 -0300

    removing old game

--- src/renderer/components/ui/activities/BaseActivity.vue
+++ src/renderer/components/ui/activities/BaseActivity.vue
@@ -165,11 +165,6 @@
                 :value-col-size="valueColSize"
                 :key-col-size="keyColSize"
             />
-            <ls-activity-read-complete-sentences
-                v-if="gameSubtypes.completeSentences === activity.subtype.slug" 
-                :value-col-size="valueColSize"
-                :key-col-size="keyColSize"
-            />
             <ls-activity-read-accentuation
                 v-if="gameSubtypes.accentuation === activity.subtype.slug" 
                 :value-col-size="valueColSize"
