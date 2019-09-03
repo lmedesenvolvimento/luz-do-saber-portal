367bcbcbeb730f3a7fd234d3b3b263e48b3b8174
eduardo <eduregis1@gmail.com>
2019-09-03 10:40:58 -0300

    removendo console.logs
    Former-commit-id: 758c4ebd852138a501062eab87b808d13f44ecd8

--- src/renderer/components/ui/activities/read/games/CompleteWord.vue
+++ src/renderer/components/ui/activities/read/games/CompleteWord.vue
@@ -5,11 +5,13 @@
                 <b-col cols="12" md="4" sm="4">
                     <async-image :src="getKeys[0].images[0].url" style="width: 200px"></async-image>
                 </b-col>
-                <b-col>
+                <b-col cols="12" md="8" sm="8">
                     <b-row>
                         <b-col v-for="(item, position) in letters" :key="position">                            
                             <div v-if="item.id == -1">
-                                <ls-card-display>                                       
+                                <ls-card-display
+                                    class="letra texto"
+                                >                                       
                                     {{ item.text }}
                                 </ls-card-display> 
                             </div>
@@ -20,7 +22,10 @@
                                     :type="'key'"
                                     :template="activity.item_template.key"
                                     :custom-validate="customValidate"
-                                >                                   
+                                >    
+                                    <template slot="transfer-data">
+                                        {{ item.text }}
+                                    </template>                              
                                 </ls-card-droppable>
                             </div>                                                      
                         </b-col>
@@ -40,65 +45,6 @@
                 </b-row>
             </ls-card-display>
         </b-col>
-        <!-- <b-row align-h="center" class="column">
-            <b-col v-if="hasKeys" class="activity-keys">
-                <b-row align-v="center">
-                    <b-col cols="12" md="4" sm="4">
-                        <async-image class="image" :src="incompleteWord.images[0].url"></async-image>
-                    </b-col>
-                    <b-col cols="12" md="8" sm="8" class="pieces-row">
-                        <b-row>
-                            <b-col v-for="(piece, index) in incompleteWord.pieces" :key="index" :md="1" class="key-pieces item">                                        
-                                <div v-if="piece.template.tags === null" :class="[piece.type, 'texto', activity.item_template.key.font_size]">
-                                    <ls-card-display
-                                        :item="piece"
-                                        :valid="piece.valid"
-                                        :invalid="piece.invalid"
-                                    >
-                                        {{ piece.text }}
-                                    </ls-card-display>                                    
-                                </div>
-                                <ls-card-droppable
-                                    v-else-if="piece.template.tags === 'encaixar'"
-                                    class="letra texto"
-                                    :item="piece"
-                                    :type="'key'"
-                                    :template="activity.item_template.key"
-                                >
-                                </ls-card-droppable>
-                            </b-col>
-                        </b-row>
-                    </b-col>
-                </b-row>
-            </b-col>
-            <b-col v-if="hasKeys" class="activity-keys">
-                <b-col class="activity-values" cols="12" md="12">
-                    <b-row align-v="center" align-h="center" class="values-container">
-                        <ls-card-display v-if="activity.item_template.value.tags==='arrastar'" class="card--display-container">
-                            <b-row align-v="center" align-h="center" cols="12" md="12">
-                                <b-col v-for="(item, position) in getValues" :key="position" align-self="center" cols="12" :sm="3" :md="3" lg="2" class="item">
-                                    <Item 
-                                        :item="item"
-                                        :type="'value'"
-                                        :template="activity.item_template.value"
-                                        :size="activity.item_template.value.font_size"
-                                    />
-                                </b-col>
-                            </b-row>
-                        </ls-card-display>
-                        <b-col v-for="(item, position) in getValues" v-else :key="position" align-self="center" cols="12" :sm="3" :md="3" lg="2" class="item" data-canTrigger="false" @click="triggerFocus(...arguments, item)">
-                            <Item 
-                                :item="item"
-                                :type="'value'"
-                                :template="activity.item_template.value"
-                                :size="activity.item_template.value.font_size"
-                                data-canTrigger="false"
-                            />                                
-                        </b-col>
-                    </b-row>
-                </b-col>
-            </b-col>
-        </b-row> -->
     </div>
 </template>
 <script>
