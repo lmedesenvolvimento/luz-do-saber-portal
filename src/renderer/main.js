256b71a90d315643e14ac925c66339947540b6fa
Damis Garcia <damisgarcia@gmail.com>
2019-09-05 20:07:35 -0300

    move boostrap to plugins folder

--- src/renderer/main.js
+++ src/renderer/main.js
@@ -1,6 +1,5 @@
 import Vue from 'vue'
 import { sync } from 'vuex-router-sync'
-import BootstrapVue from 'bootstrap-vue'
 import { VueHammer } from 'vue2-hammer'
 import axios from 'axios'
 
@@ -10,13 +9,14 @@ import store from './store'
 import './directives'
 import './plugins/v-viewer'
 import './plugins/plyr'
+import './plugins/vue-boostrap'
 
 import Assets from './services/Assets'
 
 // merge router with store
 sync(store, router)
 
-Vue.use(BootstrapVue)
+
 Vue.use(VueHammer)
 
 if (!process.env.IS_WEB) Vue.use(require('vue-electron'))
--- src/renderer/plugins/plyr.js
+++ src/renderer/plugins/plyr.js
@@ -1,3 +1,4 @@
 import Vue from 'vue'
 import VuePlyr from 'vue-plyr'
+
 Vue.use(VuePlyr)
