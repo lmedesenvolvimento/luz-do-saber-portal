3f2468dda1b84475bdde3fd24173f434bd0810fe
eduardo <eduregis1@gmail.com>
2019-09-17 08:27:43 -0300

    imagem gerada antes ou depois do conteúdo

--- src/renderer/components/ui/items/JournalPageImage.vue
+++ src/renderer/components/ui/items/JournalPageImage.vue
@@ -1,18 +1,18 @@
 <template>
     <div>
         <div             
-            class="journal-cover-image"
-            @click="$refs.coverImageInput.click()"
+            class="journal-full-image"
+            @click="$refs.imageInput.click()"
         >
             <input 
-                ref="coverImageInput"
+                ref="imageInput"
                 style="display: none"                                             
                 type="file"     
                 accept="image/gif, image/jpeg, image/png"                                         
                 @change="onFileSelectedCover"                 
             >
-            <div v-if="coverImage" class="cover-image">
-                <img :src="coverImage" alt="Imagem de capa">
+            <div v-if="image" class="full-image">
+                <img :src="image" alt="Imagem de capa">
             </div>                                       
             <div v-else class="placeholder-image">
                 <async-image src="" alt="placeholder" />
@@ -29,13 +29,13 @@ export default {
     },
     data(){
         return{
-            coverImage: ''
+            image: null
         }
     },
     methods: {
         onFileSelectedCover(event) {
             const file = event.target.files[0]
-            this.coverImage = URL.createObjectURL(file);
+            this.image = URL.createObjectURL(file);
         }, 
     }
     
