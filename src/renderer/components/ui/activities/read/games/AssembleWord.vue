7e741c7969ca05c36b2e9aa1f2516a9f3e634924
rodrigoronin <liramailbox@gmail.com>
2019-09-18 17:22:43 -0300

    Branch criado e template Default utilizado
    Former-commit-id: a3e2f0a2827e003002be8cd3d471f10039c4cd6f

---
+++ src/renderer/components/ui/activities/read/games/AssembleWord.vue
@@ -0,0 +1,63 @@
+<template>
+    <div class="container-fluid">        
+        <b-row align-h="center" :class="{'column': !horizontal,'reverse': (reverse && horizontal), 'reverse-column': ( reverse && !horizontal )}">
+            <b-col v-if="hasKeys" class="activity-keys">
+                <b-row>
+                    <b-col>
+                        <ls-card-droppable
+                            class=""
+                            :item="item"
+                            :type="'key'"
+                            :template="activity.item_template.key"
+                        >
+                        </ls-card-droppable>
+                    </b-col>
+                </b-row>
+                <b-row>
+                    <b-col v-for="(item, position) in getKeys" :key="position" :sm="keyColSize" class="item"> 
+                        <Item 
+                            v-if="answers"
+                            :item="item"
+                            :type="'key'"
+                            :template="activity.item_template.key"
+                        />
+                    </b-col>
+                </b-row>
+            </b-col>
+        </b-row>
+        <b-row>
+            <b-col
+                v-if="hasValues" 
+                :class="{ 
+                    'flex-4': getValues.length >= 12 
+                }"
+                class="activity-values" 
+            >
+                <b-row>
+                    <b-col v-for="(item, position) in getValues" :key="position" align-self="center" :md="valueColSize" :sm="6" class="item"> 
+                        <Item 
+                            v-if="answers"
+                            :item="item"
+                            :type="'value'"
+                            :template="activity.item_template.value"
+                        />
+                    </b-col>                    
+                </b-row>
+            </b-col>
+        </b-row>
+    </div>
+</template>
+<script>
+import { MapMixins, ListMixin, CreateAnswersMixins } from '@ui/activities/mixins'
+
+export default {
+    mixins: [MapMixins, ListMixin, CreateAnswersMixins],
+    mounted() {
+        this.createAnswersArray()
+    },
+}
+</script>
+
+<style>
+
+</style>
