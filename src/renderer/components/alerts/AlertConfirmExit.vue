ffe3abe85abe37f205d442f64bc283c291154767
eduardo <eduregis1@gmail.com>
2019-09-26 09:50:42 -0300

    layout do modal implementado

--- src/renderer/components/alerts/AlertConfirmExit.vue
+++ src/renderer/components/alerts/AlertConfirmExit.vue
@@ -1,20 +1,20 @@
 <template>
-    <b-modal ref="exit-modal" hide-footer content-class="confirm-exit">
+    <b-modal ref="exit-modal" centered hide-footer content-class="confirm-exit">
         <template slot="modal-header">
-            <b-col>
-                <b-row class="confirm-exit-header" align-h="center">
-                    <h2>sair</h2>
-                </b-row> 
-            </b-col>
+            <div class="confirm-exit-header">
+                <div class="confirm-exit-header-item">
+                    <h2 class="confirm-exit-header-item-text"><b>sair</b></h2>
+                </div> 
+            </div>
         </template>
-        <b-img center :src="require('@/assets/images/components/feedback/expression-star-0.png')" />  
+        <b-img class="confirm-exit-image" center :src="require('@/assets/images/components/feedback/expression-star-0.png')" />  
         <div class="confirm-exit-text">
             <h5><b>OPS!</b></h5>
-            <h5>Você realmente deseja sair?</h5>   
+            <h5>Você realmente <span class="confirm-exit-text-golden">deseja sair?</span></h5>   
         </div>
-        <div class="confirm-exit-buttons">
-            <b-img center :src="require('@/assets/images/btn-close.png')" width="61" height="61" @click="hideModal" />
-            <b-img center :src="require('@/assets/images/icons/escrever/icon-next.png')" width="61" height="61" @click="exitModal" />
+        <div class="confirm-exit-footer">
+            <div><b-img class="confirm-exit-button" :src="require('@/assets/images/btn-close.png')" @click="hideModal" /></div>
+            <div><b-img class="confirm-exit-button" :src="require('@/assets/images/icons/escrever/icon-next.png')" width="61" height="61" @click="exitModal" /></div>
         </div>
     </b-modal>
 </template>
