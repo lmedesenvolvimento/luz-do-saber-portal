503b3366147e1b31cf6754d346688fc52c65881f
rodrigoronin <liramailbox@gmail.com>
2019-10-01 15:34:55 -0300

    layout da atividade concluído. Validação precisa ser refinada, no momento funciona apenas em uma ordem específica
    Former-commit-id: 35b0951007e56f3cc07b9ea501e67efb7df91678

--- src/renderer/components/ui/activities/read/games/UppercaseLetters.vue
+++ src/renderer/components/ui/activities/read/games/UppercaseLetters.vue
@@ -4,32 +4,47 @@
             <b-col v-if="hasKeys" class="activity-keys">
                 <b-row>
                     <b-col v-for="(item, position) in getKeys" :key="position" :sm="keyColSize" class="item"> 
-                        <Item 
-                            v-if="answers"
-                            :item="item"
-                            :type="'key'"
-                            :template="activity.item_template.key"
-                        />
+                        <async-image :src="item.images.length ? item.images[0].url : ''" :alt="getKeys[0].text" />
                     </b-col>
                 </b-row>
             </b-col>
+            
             <b-col
-                v-if="hasValues" 
+                v-if="hasValues"
                 :class="{ 
                     'flex-4': getValues.length >= 12 
                 }"
                 class="activity-values" 
             >
-                <b-row>
-                    <b-col v-for="(item, position) in getValues" :key="position" align-self="center" :md="valueColSize" :sm="6" class="item"> 
-                        <Item 
-                            v-if="answers"
-                            :item="item"
-                            :type="'value'"
-                            :template="activity.item_template.value"
-                        />
-                    </b-col>                    
-                </b-row>
+                <div
+                    v-for="(item, position) in alphabet"
+                    :key="position"
+                    class="item"
+                >
+                    <div class="letra texto medium">
+                        <div class="card-input card--radio-input" :class="$attrs.class">
+                            <label>
+                                <b-card
+                                    no-body
+                                    :class="{ 'invalid': item.invalid, 'valid': item.valid }"
+                                >
+                                    <b-card-body>
+                                        {{ item.letter }}
+                                    </b-card-body>
+                                </b-card>
+
+                                <input
+                                    v-model="item.selected"
+                                    type="checkbox"
+                                    class="invisible"
+                                    true-value="valid"
+                                    false-value="invalid"
+                                    @change.stop="validate(item)"
+                                />
+                            </label>
+                        </div>
+                    </div>
+                </div>
             </b-col>
         </b-row>
     </div>
@@ -39,22 +54,89 @@ import { MapMixins, ListMixin, CreateAnswersMixins } from '@ui/activities/mixins
 import ui from '@/components/ui'
 import AsyncImage from '@ui/AsyncImage'
 import { mapState, mapActions } from 'vuex'
+import { alphabet } from '@/constants'
+import { get } from 'http'
 
 export default {
     components: {
         ...ui,
-        // AsyncImage,
+        AsyncImage,
     },
     mixins: [MapMixins, ListMixin, CreateAnswersMixins],
-    computed: {        
+    data() {
+        return {
+            alphabet: [],
+            correctLetters: [],
+            ids: [],
+        }
+    },
+    computed: {
         ...mapState('Activity', ['answers']),
     },
     mounted() {
+        alphabet.forEach((letter, index) => {
+            this.alphabet.push(Object.assign({}, {letter}))
+        })
+
+        this.getValues.forEach((item, index) => {
+            this.correctLetters.push(item.text)
+        })
+
+        for (let i = 0; i < this.activity.items.values.length; i++) {
+            const element = this.activity.items.values[i].id
+            
+            this.ids.push(element)
+        }
+        
+        
         this.createAnswersArray()
     },
+    methods: {
+        validate(item) {
+            if(item.valid || item.invalid) return
+            if(this.searchLetter(this.correctLetters, item.letter)){
+                item.valid = true
+                this.setAnswer({
+                    type: 'value',
+                    data: this.ids[0],
+                    vm: this
+                })
+
+                this.ids.pop()
+            } else {
+                item.invalid = true
+                this.setAnswer({
+                    type: 'value',
+                    data: -1,
+                    vm: this
+                })
+            }
+        },
+        searchLetter(arr, str) {
+            for(let i = 0; i < arr.length;i++){
+                if (arr[i].match(str)) return true
+            }
+            return false
+        },
+        ...mapActions('Activity', ['setActivityAttrs','setAnswer']) 
+    }
 }
 </script>
 
 <style>
-
+    .image img {
+        max-width: inherit;
+        width: 400px;
+    }
+    .image {
+        width: 300px;
+    }
+    .activity-values {
+        flex-wrap: wrap;
+        justify-content: end !important;
+        height: 80% !important;
+    }
+    .item {
+        padding-right: 10px;
+    }
 </style>
