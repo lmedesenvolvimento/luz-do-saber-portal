5a29c4ee7eeab7a8edfed0964ef9280c78e8e014
Nathan Souza <ant.nathan15@gmail.com>
2019-11-26 17:55:58 -0300

    implemented layout

--- src/renderer/components/pages/ModuleEscreverPage.vue
+++ src/renderer/components/pages/ModuleEscreverPage.vue
@@ -1,8 +1,5 @@
 <template>
-    <div
-        id="escrever"
-        class="page-container escrever"
-    >
+    <div id="escrever" class="page-container escrever">
         <navbar
             :navbar-title="getModule.title"
             :navbar-subtitle="''"
@@ -10,8 +7,11 @@
         />
         <div v-if="getModule.key === 'jornal'">
             <journal></journal>
-        </div>               
-        <div v-else class="page-container-wrap-spacing">    
+        </div>
+        <div v-else-if="getModule.key === 'cartao'">
+            <convite></convite>
+        </div>
+        <div v-else class="page-container-wrap-spacing">
             <div v-if="getModule.key === 'editor'">
                 <editor></editor>
             </div>
@@ -21,25 +21,24 @@
         </div>
     </div>
 </template>
-
 <script>
 import Navbar from '../ui/navbars/Navbar'
 import Journal from '../ui/items/Journal'
 import Editor from '../ui/items/Editor'
+import Convite from '../ui/items/Convite'
 export default {
-    components: { Navbar, Journal, Editor},
+    components: { Navbar, Journal, Editor, Convite },
     computed: {
-        getModuleImage(){
+        getModuleImage() {
             return require('@/assets/images/btn-write.png')
-        },        
-        getModule(){
+        },
+        getModule() {
             console.log(this.$route.params)
-            return this.$store.getters['WriteModule/getWriteModuleByRef'](this.$route.params.escrever_id)
+            return this.$store.getters['WriteModule/getWriteModuleByRef'](
+                this.$route.params.escrever_id
+            )
         }
     }
 }
 </script>
-
-<style>
-    
-</style>
+<style></style>
