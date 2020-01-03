442faccc1ba74896a63d4f6ec1f9ff99d92db179
rodrigoronin <liramailbox@gmail.com>
2020-01-03 09:51:20 -0300

    Cache das atividades removido
    Former-commit-id: c271bcecf6bb5d9ceaec0af1ac91f9b6ebc16847

--- src/renderer/services/Http.js
+++ src/renderer/services/Http.js
@@ -1,12 +1,18 @@
 import axios from 'axios'
-import { setupCache } from 'axios-cache-adapter'
 
-const cache = setupCache({
-    maxAge: 15 * 60 * 1000
-})
+// ! DEPRECATED
+// import { setupCache } from 'axios-cache-adapter'
+
+// const cache = setupCache({
+//     maxAge: 15 * 60 * 1000
+// })
+
+// const api = axios.create({
+//     adapter: cache.adapter,
+//     baseURL: process.env.BUILD_TARGET === 'web' ? process.env.BASE_API_URL : `${process.env.BASE_API_URL}/api`
+// })
 
 const api = axios.create({
-    adapter: cache.adapter,
     baseURL: process.env.BUILD_TARGET === 'web' ? process.env.BASE_API_URL : `${process.env.BASE_API_URL}/api`
 })
 
