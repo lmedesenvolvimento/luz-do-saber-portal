c900dcd01be8256d693e1ad0070b2c6eb26385da
Nathan Souza <ant.nathan15@gmail.com>
2020-01-06 14:04:50 -0300

    implemented drag and drop
    Former-commit-id: fb348791fc6bfde99b045f6655071ea1226d51f0

---
+++ src/renderer/components/ui/items/Drop.vue
@@ -0,0 +1,135 @@
+<template>
+    <div class="drop-el">
+        <slot />
+    </div>
+</template>
+<script>
+import interact from 'interactjs'
+export default {
+    props: {
+        expected: {
+            type: [String, Object],
+            required: true
+        },
+        selectedObjects: {
+            type: Array,
+            required: true
+        },
+        replaceOndrop: {
+            type: Function,
+            required: false,
+            default: null
+        },
+        snap: {
+            type: Object,
+            required: false,
+            default() {
+                return undefined
+            }
+        }
+    },
+    data() {
+        return {
+            dropped: [],
+            initialSnapTarget: {
+                targets: [{ x: 0, y: 0, range: Infinity }],
+                relativePoints: [
+                    { x: 0, y: 0 } // snap relative to the element's top-left,
+                ],
+                enabled: true,
+                endOnly: true,
+                offset: 'self'
+            },
+            dropzoneSnap: this.snap || { x: 0, y: 0 }
+        }
+    },
+    mounted() {
+        const snap = this.initialSnapTarget
+        const droppedArr = this.dropped
+        interact(this.$el).dropzone({
+            accept: '.drag-el',
+            overlap: 0.25,
+            checker(dragEvent, event, dropped) {
+                return dropped && !droppedArr.length > 0
+            },
+            snap: {
+                enabled: false
+            },
+            ondropactivate: (event) => this.ondropactivate(event),
+            ondragenter: (event) => this.ondragenter(event),
+            ondragleave: (event) => this.ondragleave(event, snap),
+            ondrop: (event) => this.ondrop(event, droppedArr, snap),
+            ondropdeactivate: (event) => this.ondropdeactivate(event)
+        })
+    },
+    methods: {
+        ondropactivate(event) {
+            event.target.classList.add('drop-active')
+        },
+        ondragenter(event) {
+            const draggableElement = event.relatedTarget
+            const startPosX =
+                parseFloat(draggableElement.getAttribute('data-start-x')) || 0
+            const startPosY =
+                parseFloat(draggableElement.getAttribute('data-start-y')) || 0
+            const dropRect = interact.getElementRect(event.target)
+            const dropCenter = {
+                x:
+                    dropRect.left -
+                    startPosX +
+                    this.dropzoneSnap.x +
+                    dropRect.width / 2,
+                y:
+                    dropRect.top -
+                    startPosY +
+                    this.dropzoneSnap.y +
+                    dropRect.height / 2,
+                range: Infinity
+            }
+            event.draggable.draggable({
+                snap: {
+                    targets: [dropCenter],
+                    relativePoints: [{ x: 0, y: 0 }],
+                    enabled: true,
+                    endOnly: true
+                }
+            })
+            event.target.classList.add('drop-target')
+            event.relatedTarget.classList.add('can-drop')
+        },
+        ondragleave(event, snap) {
+            event.target.classList.remove('drop-target')
+            event.relatedTarget.classList.remove('can-drop')
+            event.draggable.draggable({ snap })
+        },
+        ondrop(event, dropped, snap) {
+            event.draggable.draggable({ enabled: false })
+            const data = JSON.parse(
+                event.relatedTarget.getAttribute('data-transfer')
+            )
+            dropped.push(data)
+            event.relatedTarget.offsetParent.offsetParent.style.transform =
+                'rotate(0deg)'
+            if (data.dropped !== undefined && !data.dropped) {
+                data.dropped = true
+            }
+        },
+        ondropdeactivate(event) {
+            event.target.classList.remove('drop-active')
+            event.target.classList.remove('drop-target')
+        }
+    }
+}
+</script>
+<style lang="scss">
+.drop {
+    position: relative;
+    @include flex-center;
+    width: 222px;
+    height: 218px;
+
+    &.over {
+        filter: brightness(90%);
+    }
+}
+</style>
