bdd56c197dc95e4fc8712c835c374467daffa5ed
joaovictoralencar <joaovictoralencarpascoal@gmail.com>
2020-02-05 12:18:53 -0300

    ajustando layout quebrando
    Former-commit-id: 6de875faecfd01361e3c26528dc4f9d39ad31684

--- src/renderer/components/ui/activities/read/games/DraggingSyllables.vue
+++ src/renderer/components/ui/activities/read/games/DraggingSyllables.vue
@@ -1,99 +1,96 @@
 <template>
     <div id="dragging-syllables" class="container-fluid">
-        <b-row align-h="center" class="reverse">
-            <b-col v-if="hasKeys" class="activity-keys">
-                <b-row align-v="center" align-h="center">
-                    <b-col
-                        v-for="(item, position) in newItens"
-                        :key="position"
-                        cols="12"
-                    >
-                        <b-row class="custom-row" align-v="center">
-                            <b-col class="image-container">
-                                <ls-card-display>
-                                    <async-image
-                                        class="image"
-                                        :src="item.images[0].url"
-                                    ></async-image>
-                                </ls-card-display>
-                            </b-col>
-                            <b-col class="no-padding">
-                                <b-row
-                                    align-h="start"
-                                    class="syllables-row custom-row"
+        <b-container>
+            <b-row align-h="center" align-v="center">
+                <b-col class="activity-values" md="auto">
+                    <ls-card-display id="values-container-display">
+                        <b-row class="values-container" align-h="center">
+                            <b-col
+                                v-for="(item, position) in shuffleValues"
+                                :key="position"
+                                class="item"
+                                cols="auto"
+                            >
+                                <div
+                                    class="silaba texto"
+                                    :class="activity.item_template.value.font_size"
                                 >
-                                    <b-col
-                                        v-for="(syllables,
-                                                index) in item.syllables"
-                                        :key="index"
-                                        class="item key-syllables"
+                                    <ls-card-draggable
+                                        v-if="answers"
+                                        class="value"
+                                        :item="item"
+                                        :type="'value'"
+                                        :template="activity.item_template.value"
                                     >
-                                        <div
-                                            class="silaba texto validate-icon-top"
-                                            :class="
-                                                activity.item_template.key
-                                                    .font_size
-                                            "
-                                        >
-                                            <ls-card-droppable
-                                                v-if="answers"
-                                                :item="syllables.syllable"
-                                                :type="'key'"
-                                                :template="
-                                                    activity.item_template.key
-                                                "
-                                                :custom-validate="
-                                                    validateBySyllabe
-                                                "
-                                            >
-                                                <template slot="transfer-data">
-                                                    <div>
-                                                        {{
-                                                            syllables.correct
-                                                                ? syllables
-                                                                    .syllable
-                                                                    .text
-                                                                : dataTransfer.text
-                                                        }}
-                                                    </div>
-                                                </template>
-                                            </ls-card-droppable>
-                                        </div>
-                                    </b-col>
-                                </b-row>
+                                        {{ item.text }}
+                                    </ls-card-draggable>
+                                </div>
                             </b-col>
                         </b-row>
-                    </b-col>
-                </b-row>
-            </b-col>
-            <b-col class="activity-values">
-                <ls-card-display id="values-container-display">
-                    <b-row class="values-container" align-h="center">
-                        <b-col
-                            v-for="(item, position) in shuffleValues"
-                            :key="position"
-                            align-self="center"
-                            class="item value-syllables"
-                        >
-                            <div
-                                class="silaba texto"
-                                :class="activity.item_template.value.font_size"
+                    </ls-card-display>
+                </b-col>
+                <b-col v-if="hasKeys" class="activity-keys" md="auto">
+                    <b-row v-for="(item, position) in newItens"
+                           :key="position" align-v="center"
+                    >
+                        <b-col class="image-container" md="auto">
+                            <ls-card-display>
+                                <async-image
+                                    class="image"
+                                    :src="item.images[0].url"
+                                ></async-image>
+                            </ls-card-display>
+                        </b-col>
+                        <b-col>
+                            <b-row
+                                align-h="start"
+                                class="syllables-row custom-row"
                             >
-                                <ls-card-draggable
-                                    v-if="answers"
-                                    class="value"
-                                    :item="item"
-                                    :type="'value'"
-                                    :template="activity.item_template.value"
+                                <b-col
+                                    v-for="(syllables,
+                                            index) in item.syllables"
+                                    :key="index"
+                                    class="item key-syllables"
+                                    md="auto"
                                 >
-                                    {{ item.text }}
-                                </ls-card-draggable>
-                            </div>
+                                    <div
+                                        class="silaba texto validate-icon-top"
+                                        :class="
+                                            activity.item_template.key
+                                                .font_size
+                                        "
+                                    >
+                                        <ls-card-droppable
+                                            v-if="answers"
+                                            :item="syllables.syllable"
+                                            :type="'key'"
+                                            :template="
+                                                activity.item_template.key
+                                            "
+                                            :custom-validate="
+                                                validateBySyllabe
+                                            "
+                                        >
+                                            <template slot="transfer-data">
+                                                <div>
+                                                    {{
+                                                        syllables.correct
+                                                            ? syllables
+                                                                .syllable
+                                                                .text
+                                                            : dataTransfer.text
+                                                    }}
+                                                </div>
+                                            </template>
+                                        </ls-card-droppable>
+                                    </div>
+                                </b-col>
+                            </b-row>
                         </b-col>
                     </b-row>
-                </ls-card-display>
-            </b-col>
-        </b-row>
+                </b-col>
+            </b-row>
+        </b-container>
     </div>
 </template>
 <script>
