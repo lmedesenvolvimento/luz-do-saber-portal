0462746e5bae7d680a2cc8ad11d59fbe31087ccc
damisgarcia@gmail.com <damisgarcia@gmail.com>
2020-02-13 13:04:56 -0300

    adicionado eventos de respostas do bingo, e resolvido bug letras com acento
    Former-commit-id: 69cfb80914fe13c708920613300ca942718c76ed

--- src/renderer/components/ui/activities/begin/games/Bingo.vue
+++ src/renderer/components/ui/activities/begin/games/Bingo.vue
@@ -7,21 +7,52 @@
                         Bingo! 
                         <span class="countdown" :class="{'is-sorting': sorting}">{{ countDown }}s</span>
                     </p>
+                    <h2>
+                        {{ sorted }}
+                    </h2>
                     <p>
                         Alphabet: <br>
                         {{ alphabet.map(l => l.text).join(', ') }}
                     </p>
                 </div>
-                <div class="col">
+                <div class="col-9">
                     <div class="bingo-opponents container-fluid">
-                        bingo-opponents
-                        <div v-for="value in getValues" :key="value.id" class="row">
-                            <div v-for="(l, index) in value.text.split('')" :key="index.toString()" class="col-1">
-                                <div class="bingo-opponent" :class="{'is-sorted': getLetter(l).sorted}">
-                                    {{ getLetter(l).text }}
+                        <ls-card-display>
+                            Sua Cartela
+                            <div v-for="key in getKeys" :key="key.id" class="row bingo-player-letters">
+                                <div v-for="(l, index) in splitLetters(key.text)" :key="index.toString()" class="bingo-player-letter">
+                                    <div class="item">
+                                        <div class="letra texto medium">
+                                            <div :id="`input-${index}`" class="card-input card--radio-input">
+                                                <b-card 
+                                                    no-body
+                                                    :class="{ 'invalid': getLetter(l).invalid, 'valid': getLetter(l).valid }"
+                                                    @click.stop="validateLetter(l)"
+                                                >
+                                                    <b-card-body>
+                                                        {{ getLetter(l).text }}
+                                                    </b-card-body>
+                                                </b-card>
+                                            </div>
+                                        </div>
+                                    </div>                                    
                                 </div>
                             </div>
-                        </div>                            
+                        </ls-card-display>
+                        
+                        <ls-card-display v-for="value in opponents" :key="value.id"> 
+                            <div class="row bingo-opponents-letters">
+                                <div v-for="(l, index) in splitLetters(value.text)" :key="index.toString()" class="bingo-opponents-letter">
+                                    <div class="item">
+                                        <div class="letra texto medium">
+                                            <ls-card-display :valid="getLetter(l).sorted">
+                                                {{ getLetter(l).text }}
+                                            </ls-card-display>
+                                        </div>
+                                    </div>
+                                </div>
+                            </div>
+                        </ls-card-display>
                     </div>
                 </div>
             </div>
@@ -29,19 +60,35 @@
     </div>
 </template>
 <script>
-import { find, flattenDeep, sample, debounce, pick, uniq } from 'lodash'
+import { mapActions } from 'vuex'
+import { 
+    find, 
+    flattenDeep, 
+    sample, 
+    debounce, 
+    pick, 
+    uniq, 
+    deburr, 
+    clone
+} from 'lodash'
 import  { alphabet, alphabet_with_acents } from '@/constants'
 
 import { MapMixins, ListMixin, CreateAnswersMixins } from '@ui/activities/mixins'
 
+import ui from '@/components/ui'
+
 const TIMEOUT = 5
 
 export default {
+    components: {
+        ...ui
+    },
     mixins: [MapMixins, ListMixin, CreateAnswersMixins],
     data(){
         return {
             ready: false,
             sorting: true,
+            sorted: null,
             countDown: TIMEOUT,
             countDownId: null,
             alphabet: alphabet.map((l, index) => {
@@ -53,16 +100,13 @@ export default {
         }
     },
     computed: {
-        sorted() {
-            return this.alphabet.filter(l => l.sorted)
-        },
         sortLetters(){
-            const keyLetters = this.getKeys.map(({ letters }) => {
-                return letters.map(l => l.text)
+            const keyLetters = this.getKeys.map(({ text }) => {
+                return deburr(text).split('')
             })
 
-            const valueLetters = this.getValues.map(({ letters }) => {
-                return letters.map(l => l.text)
+            const valueLetters = this.getValues.map(({ text }) => {
+                return deburr(text).split('')
             })
 
             const flattenValues = flattenDeep(valueLetters)
@@ -73,6 +117,9 @@ export default {
             return this.alphabet.filter(({ text }) => {
                 return letters.includes(text)
             })
+        },
+        opponents() {
+            return this.getValues.filter((value) => !value.key_id)
         }
     },
     created() {
@@ -98,8 +145,13 @@ export default {
                 return
             }
 
+            // console.log(sorted.id)
+
+            this.sorted = sorted.text
+
             sorted.sorted = true
             this.$set(this.alphabet, sorted.id, sorted)
+            // this.$set(this, 'sorted', sorted.id)
         },
         countdown(){
             if (!this.sorting) {
@@ -127,6 +179,7 @@ export default {
         start: debounce(function(){
             console.log('Sorting...')
             this.sorting = true
+            this.sorted = null
             this.countDownId = setInterval(this.countdown, 1000)
         }, 3000),
         
@@ -148,12 +201,51 @@ export default {
                 })
             })
         },
-        getLetter(text) {
-            const indexOf = this.alphabet.findIndex(l => {
+        splitLetters (text) {
+            return deburr(text).split('')
+        },
+        getLetterIndexOf(text) {
+            return this.alphabet.findIndex(l => {
                 return l.text === text
             })
+        },
+        getLetter(text) {
+            const indexOf = this.getLetterIndexOf(text)
             return this.alphabet[indexOf] || {}
-        }
+        },
+        validateLetter(text) {
+            let valid = []
+            const letter = this.getLetter(text)
+            const alphabet = clone(this.alphabet)
+            
+            this.getKeys.forEach((key) => {
+                valid.push(letter.sorted)
+            })
+
+            const indexOf = this.getLetterIndexOf(text)
+
+            if (valid.every(i => !!i)) {
+                alphabet[indexOf].valid = true
+            } else {
+                alphabet[indexOf].invalid = true
+    
+                this.setAnswer({
+                    type: 'value',
+                    data: -1,
+                    vm: {}
+                })
+            }
+
+
+            this.$set(this, 'alphabet', alphabet)
+
+            setTimeout(() => {
+                if (alphabet[indexOf].invalid) {
+                    delete alphabet[indexOf].invalid
+                }
+            }, 1000)
+        },
+        ...mapActions('Activity', ['setAnswer'])
     }
 }
 </script>
@@ -163,11 +255,13 @@ export default {
     .countdown:not(.is-sorting) {
         color: red;
     }
-    .bingo-opponents {
-        .bingo-opponent {
-            &.is-sorted {
-                color: green !important;
-            }
+    .bingo-opponents, .bingo-player {
+        &-letters {
+            justify-content: center;
+        }
+        &-letter {
+            padding-right: $gutter-size;
+            padding-left: $gutter-size;
         }
     }
 }
