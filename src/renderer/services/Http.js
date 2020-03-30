4e9cf58fbb19f8ea9e430779e916ac777a323699
Damis Garcia <damisgarcia@gmail.com>
2020-03-30 18:36:18 -0300

    atualizado esquema para resgate da url da api

--- src/renderer/services/Http.js
+++ src/renderer/services/Http.js
@@ -1,5 +1,9 @@
 import axios from 'axios'
 
+let Http = {
+    axios: {}
+}
+
 // ! DEPRECATED
 // import { setupCache } from 'axios-cache-adapter'
 
@@ -12,10 +16,33 @@ import axios from 'axios'
 //     baseURL: process.env.BUILD_TARGET === 'web' ? process.env.BASE_API_URL : `${process.env.BASE_API_URL}/api`
 // })
 
-console.log(process.env.BASE_API_URL)
+export function asyncAxios(callback) {
+    console.log(process.env.BUILD_TARGET, process.env.NODE_ENV)
+    if (process.env.BUILD_TARGET === 'web' && process.env.NODE_ENV === 'production') {
+        axios.get('/site').then(({ data }) => {
+            Http.axios = axios.create({
+                baseURL: data.api_url
+            })
+    
+            if (callback) return callback()
+        })
+    }
 
-const api = axios.create({
-    baseURL: process.env.BUILD_TARGET === 'web' ? process.env.BASE_API_URL : `${process.env.BASE_API_URL}/api`
-})
+    else if (process.env.BUILD_TARGET === 'web') {
+        Http.axios = axios.create({
+            baseURL: `${process.env.BASE_API_URL}`
+        })
+        
+        if (callback) return callback()
+    }
 
-export default api
+    Http.axios = axios.create({
+        baseURL: `${process.env.BASE_API_URL}/api`
+    })
+
+    if (callback) return callback()
+}
+
+
+
+export default Http
