8bf206f80b97aaa4e2851f9595b046eb5614d06c
Jose Magalhaes <josemagalhaesnt@gmail.com>
2021-05-06 09:02:32 -0300

    Updated image loading animation
    Former-commit-id: 66b54e337aafbf0f486a1e1a88f8707e74d8a06e

--- src/renderer/components/ui/form/CardImageDisplay.vue
+++ src/renderer/components/ui/form/CardImageDisplay.vue
@@ -7,7 +7,7 @@
         >
             <b-card-body>
                 <slot name="img">
-                    <async-image class="card-img" :src="picture" :alt="imgAlt" />
+                    <async-image class="card-img" color="#999" :src="picture" :alt="imgAlt" />
                 </slot>
                 <slot></slot>
             </b-card-body>
